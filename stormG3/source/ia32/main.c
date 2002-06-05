f9726f21fb685667fa97411b5a6ae455bd45b64d
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-05 19:56:07 +0000

    Added physical memory initialization.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@109 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/source/ia32/main.c
+++ stormG3/source/ia32/main.c
@@ -1,4 +1,4 @@
-/* $chaos: main.c,v 1.4 2002/05/26 10:59:18 hal Exp $ */
+/* $chaos: main.c,v 1.5 2002/05/27 06:22:50 per Exp $ */
 /* Abstract: This is the startup point of storm. It is executed right
    after the assembly language init code has set up the GDT, kernel
    stack, etc. Here, we initialise everything in the storm, like
@@ -13,17 +13,42 @@
 /* Use freely under the terms listed in the file COPYING.
    reference documentation. */
 
+#include <storm/config.h>
 #include <storm/defines.h>
 #include <storm/ia32/debug.h>
 #include <storm/ia32/main.h>
-#include <storm/config.h>
+#include <storm/ia32/memory_physical.h>
+#include <storm/ia32/multiboot.h>
 
 /* Do the bootup procedure. */
 void main_bootup (int argument_count UNUSED, char *arguments[] UNUSED)
 {
+    void *pointer1 = NULL, *pointer2 = NULL, *pointer3 = NULL;
+
     /* Set up debugging. */
-    debug_init();
-    
+    debug_init ();
     debug_print ("storm %s (compiled by %s on %s %s).\n", STORM_VERSION, CREATOR, __DATE__, __TIME__);
-    while (TRUE);
+
+    /* Initialize the multiboot structures. */
+    multiboot_init ();
+    
+    /* Set up physical memory allocation. */
+    memory_physical_init ();
+
+    /* Test the allocation. */
+    debug_print ("Return value: %d", memory_physical_allocate(&pointer1, 1));
+    debug_print ("%x\n", pointer1);
+    debug_print ("Return value: %d ", memory_physical_allocate(&pointer2, 1));
+    debug_print ("%x\n", pointer2);
+    debug_print ("Return value: %d ", memory_physical_allocate(&pointer3, 1));
+    debug_print ("%x\n", pointer3);
+    memory_physical_deallocate(pointer1);
+    debug_print ("Return value: %d ", memory_physical_allocate(&pointer1, 1));
+    debug_print ("%x\n", pointer1);
+    debug_print ("Return value: %d ", memory_physical_allocate(&pointer2, 1));
+    debug_print ("%x\n", pointer2);
+    debug_print ("Return value: %d ", memory_physical_allocate(&pointer3, 1));
+    debug_print ("%x\n", pointer3);
+
+    /* We are finished. Pass on to the idle task. */
 }
