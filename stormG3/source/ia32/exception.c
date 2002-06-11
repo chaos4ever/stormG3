2aa0ad074c76715849ca0ff2391a31966d242b4a
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-11 21:06:57 +0000

    New file: Exception handling.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@132 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ stormG3/source/ia32/exception.c
@@ -0,0 +1,11 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: Exception handling. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#include <storm/ia32/exception.h>
+
+void exception_init () {
+}
