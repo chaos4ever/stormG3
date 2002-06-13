b5baee9cec2816effe1b8fdfeb84c808c0f1b734
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-13 07:02:30 +0000

    New type: exception_handler_t as well as many new prototypes.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@167 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/exception.h
+++ stormG3/include/storm/ia32/exception.h
@@ -1,4 +1,4 @@
-/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* $chaos: exception.h,v 1.1 2002/06/11 21:05:57 per Exp $ */
 /* Abstract: Exception handling. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -8,6 +8,39 @@
 #ifndef __STORM_IA32_EXCEPTION_H__
 #define __STORM_IA32_EXCEPTION_H__
 
+#include <storm/ia32/cpu.h>
+
+/* Types. */
+typedef struct
+{
+    void *function;
+} exception_handler_t;
+
+/* Initialize exception handling. */
 extern void exception_init (void);
+
+/* Exception handlers prototypes. Low-level first: */
+extern void exception_divide_error_fault_lowlevel (void);
+extern void exception_debug_trap_lowlevel (void);
+extern void exception_nmi_lowlevel (void);
+extern void exception_breakpoint_trap_lowlevel (void);
+extern void exception_overflow_trap_lowlevel (void);
+extern void exception_bound_range_exceeded_fault_lowlevel (void);
+extern void exception_invalid_opcode_fault_lowlevel (void);
+extern void exception_device_not_available_fault_lowlevel (void);
+extern void exception_double_fault_lowlevel (void);
+extern void exception_coprocessor_segment_overrun_abort_lowlevel (void);
+extern void exception_invalid_tss_fault_lowlevel (void);
+extern void exception_segment_not_present_fault_lowlevel (void);
+extern void exception_stack_fault_lowlevel (void);
+extern void exception_general_protection_fault_lowlevel (void);
+extern void exception_page_fault_lowlevel (void);
+extern void exception_dummy_lowlevel (void);
+extern void exception_floating_point_error_fault_lowlevel (void);
+extern void exception_alignment_check_fault_lowlevel (void);
+extern void exception_machine_check_abort_lowlevel (void);
+
+/* ...and the C functions. */
+extern void exception_page_fault (cpu_register_t *registers);
 
 #endif /* !__STORM_IA32_EXCEPTION_H__ */
