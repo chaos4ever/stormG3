5f63a906e85f90425fc08dd03c3dca6dabd1b529
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-15 14:36:12 +0000

    Removed fixed-address data, more error checking.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@212 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/source/ia32/multiboot.c
+++ stormG3/source/ia32/multiboot.c
@@ -1,4 +1,4 @@
-/* $chaos: multiboot.c,v 1.2 2002/06/05 18:17:26 per Exp $ */
+/* $chaos: multiboot.c,v 1.3 2002/06/15 12:09:15 per Exp $ */
 /* Abstract: Parse multiboot header. */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
@@ -7,6 +7,7 @@
 /* Use freely under the terms listed in the file COPYING.
    reference documentation. */
 
+#include <storm/ia32/debug.h>
 #include <storm/ia32/defines.h>
 #include <storm/ia32/memory.h>
 #include <storm/ia32/multiboot.h>
@@ -14,14 +15,14 @@
 
 multiboot_info_type multiboot_info;
 multiboot_module_info_type multiboot_module_info[MAX_STARTUP_SERVERS];
-void *memory_map = (void *) MEMORY_MAP_BASE;
+static uint8_t memory_map[MEMORY_MAP_SIZE];
+static char module_name[MODULE_NAME_SIZE];
 
 /* Copy off some of the Multiboot data we need, so it won't be
    overwritten. */
 void multiboot_init (void)
 {
-    char *target = (char *) MODULE_NAME_BASE;
-    unsigned module;
+    char *target = (char *) &module_name;
 
     /* Make sure we don't get out of bounds. */
     if (multiboot_info.number_of_modules > MAX_STARTUP_SERVERS)
@@ -38,16 +39,23 @@ void multiboot_init (void)
     /* First of all, make sure module names and parameters are stored
        in a safe place. Otherwise, we may overwrite them later on in
        the boot process. */
-    for (module = 0; module < multiboot_info.number_of_modules; module++)
+    for (unsigned int module = 0; module < multiboot_info.number_of_modules;
+         module++)
     {
-        string_copy (target, multiboot_module_info[module].name);
-        multiboot_module_info[module].name = target;
-        target += string_length (target) + 1;
+        if (multiboot_module_info[module].name == NULL) 
+        {
+            multiboot_module_info[module].name = NULL;
+        }
+        else
+        {
+            string_copy (target, multiboot_module_info[module].name);
+            multiboot_module_info[module].name = target;
+            target += string_length (target) + 1;
+        }
     }
     
     /* Now, save the memory map. */
-    memory_copy (memory_map, 
-                 (void *) multiboot_info.memory_map_address, 
+    memory_copy (&memory_map, (void *) multiboot_info.memory_map_address, 
                  multiboot_info.memory_map_length);
     
     /* Now, lets parse the kernel command line. */
