1ec92c77e77abb68361db1384d0bd741d39fa7b1
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-15 11:00:03 +0000

    Added call to irq_init.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@200 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/source/ia32/main.c
+++ stormG3/source/ia32/main.c
@@ -1,4 +1,4 @@
-/* $chaos: main.c,v 1.12 2002/06/13 20:07:52 per Exp $ */
+/* $chaos: main.c,v 1.13 2002/06/14 12:40:28 per Exp $ */
 /* Abstract: This is the startup point of storm. It is executed right
    after the assembly language init code has set up the GDT, kernel
    stack, etc. Here, we initialise everything in the storm, like
@@ -19,6 +19,7 @@
 #include <storm/ia32/debug.h>
 #include <storm/ia32/dma.h>
 #include <storm/ia32/exception.h>
+#include <storm/ia32/irq.h>
 #include <storm/ia32/main.h>
 #include <storm/ia32/memory_global.h>
 #include <storm/ia32/memory_physical.h>
@@ -42,7 +43,10 @@ void main_bootup (int argument_count UNUSED, char *arguments[] UNUSED)
 
     /* Initialize the multiboot structures. */
     multiboot_init ();
-    
+
+    /* Set up exception handlers. */
+    exception_init ();
+
     /* Set up physical memory allocation. */
     memory_physical_init ();
 
@@ -55,47 +59,15 @@ void main_bootup (int argument_count UNUSED, char *arguments[] UNUSED)
     /* Set up DMA support. */
     dma_init ();
 
-    /* Set up exception handlers. */
-    exception_init ();
-
-    debug_print ("CPU vendor name: %s\n", cpu_info.name);
+    /* Set up IRQ support. This will also enable interrupts, so the
+       dispatcher will be called. */
+    irq_init ();
 
 #ifdef GDB_INCLUDE
     serial_init (GDB_PORT, GDB_SPEED);
     set_debug_traps ();
     BREAKPOINT ();
 #endif
-
-    *(int *)(NULL) = 0;
-
-    /* Test the allocation. */
-    {
-        void *pointer1 = NULL, *pointer2 = NULL, *pointer3 = NULL;
-        return_t return_value;
-        return_value = memory_global_allocate (&pointer1, 64);
-        debug_print ("[%d, %x] ", return_value, pointer1);
-
-        return_value = memory_global_allocate (&pointer2, 64);
-        debug_print ("[%d, %x] ", return_value, pointer2);
-
-        return_value = memory_global_allocate (&pointer3, 64);
-        debug_print ("[%d, %x] ", return_value, pointer3);
-
-        return_value = memory_global_deallocate (pointer1);
-        debug_print ("[%d] ", return_value);
-
-        return_value = memory_global_deallocate (pointer2);
-        debug_print ("[%d] ", return_value);
-
-        return_value = memory_global_allocate (&pointer2, 64);
-        debug_print ("[%d, %x] ", return_value, pointer2);
-
-        return_value = memory_global_deallocate (pointer3);
-        debug_print ("[%d] ", return_value);
-
-        return_value = memory_global_deallocate (pointer2);
-        debug_print ("[%d] ", return_value);
-    }
 
     /* We are finished. Pass on to the idle task. */
 }
