f70d7f594d3df681a2bb042b8ceb242305f6eebd
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-15 13:51:02 +0000

    New files (pretty empty yet)
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@208 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ stormG3/include/storm/ia32/process.h
@@ -0,0 +1,17 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: Process support. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __STORM_IA32_PROCESS_H__
+#define __STORM_IA32_PROCESS_H__
+
+/* The process ID of the kernel process (idle thread). */
+#define PROCESS_ID_KERNEL               0
+
+/* Type definitions. */
+typedef unsigned int process_id_t;
+
+#endif /* !__STORM_IA32_PROCESS_H__ */
---
+++ stormG3/include/storm/ia32/thread.h
@@ -0,0 +1,17 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: Thread support. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __STORM_IA32_THREAD_H__
+#define __STORM_IA32_THREAD_H__
+
+/* The thread ID of the kernel thread. (idle thread) */
+#define THREAD_ID_KERNEL                0
+
+/* Type definitions. */
+typedef unsigned int thread_id_t;
+
+#endif /* !__STORM_IA32_THREAD_H__ */
