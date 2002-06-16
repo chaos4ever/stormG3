64ba8e7b947409f10919e9f96650da32cb4e13ef
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-16 21:18:18 +0000

    New file.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@236 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ stormG3/include/storm/ia32/types.h
@@ -0,0 +1,16 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: IA32-specific types. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __STORM_IA32_TYPES_H__
+#define __STORM_IA32_TYPES_H__
+
+#include <storm/types.h>
+
+/* A linear/physical address. */
+typedef uint32_t address_t;
+
+#endif /* !__STORM_IA32_TYPES_H__ */
