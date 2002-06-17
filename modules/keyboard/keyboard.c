00c8c238c98f28ea82f48478bb91363a1db93dcb
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-17 07:27:27 +0000

    Very ugly, but it works. Should be rewrited to incorporate the old keyboard server ASAP.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@252 1f819a86-672b-0410-b5ed-9de735de380c

--- modules/keyboard/keyboard.c
+++ modules/keyboard/keyboard.c
@@ -1,14 +1,28 @@
-/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* $chaos: keyboard.c,v 1.1 2002/06/16 21:08:00 per Exp $ */
 /* Abstract: Keyboard module. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file COPYING. */
 
-//extern void module_register (void);
 extern void module_start (void);
+
+/* FIXME: Include a kernel header file to get these. */
+typedef void (irq_handler_t)(unsigned int irq_level);
+
+extern void debug_print (const char *format_string, ...);
+extern void module_register (void);
+extern unsigned int irq_register (unsigned int irq_number, char *description,
+                                  irq_handler_t *function);
+
+static void irq1_handler (unsigned int irq_number)
+{
+    debug_print ("%u\n", irq_number);
+    asm ("inb $0x60, %al");
+}
 
 void module_start (void)
 {
-    //    module_register ();
+    module_register ();
+    debug_print ("%u\n", irq_register (1, "Keyboard interrupt", &irq1_handler));
 }
