58addf9da79a7d1679ebfa6b3b9b487cc6a0aa39
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-18 22:13:58 +0000

    Added a tick counter.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@303 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/source/ia32/dispatch.c
+++ stormG3/source/ia32/dispatch.c
@@ -1,4 +1,4 @@
-/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* $chaos: dispatch.c,v 1.1 2002/06/15 10:47:46 per Exp $ */
 /* Abstract: Dispatcher. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -8,11 +8,15 @@
 #include <storm/ia32/defines.h>
 #include <storm/ia32/dispatch.h>
 
+/* The number of ticks since IRQ0 was first enabled. */
+volatile unsigned int ticks = 0;
+
 /* The task switcher -- IRQ0 handler. */
 void dispatch_task_switcher (void)
 {
     /* Return straight away for now. */
-    asm ("notl 0xB8000\n"
+    asm ("incl ticks\n"
+         "notl 0xB8000\n"
          "movb $0x20, %al\n"
          "outb %al, $0x20");
     asm ("iret");
