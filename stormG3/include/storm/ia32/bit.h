ffb8aeb71aa8effb1d4d3b0735fa7327bf68f5ad
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-18 19:29:19 +0000

    New macro: LOW_8 New macro: HIGH_8
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@288 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/bit.h
+++ stormG3/include/storm/ia32/bit.h
@@ -1,4 +1,4 @@
-/* $chaos: bit.h,v 1.2 2002/06/05 18:17:42 per Exp $ */
+/* $chaos: bit.h,v 1.3 2002/06/08 15:12:34 per Exp $ */
 /* Abstract: Bit manipulation macros. */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
@@ -11,18 +11,22 @@
 
 /* This file is endian-specific. */
 
-/* Get the low or high 16 bits of a 32 bit integer
-   variable. (Endian-specific) */
+/* Get the low or high 16 bits of a 32-bit integer
+   variable. */
 #define LOW_16(x)                       ((x) & 0xFFFF)
 #define HIGH_16(x)                      ((x) >> 16)
 
+/* Get the low or high 8 bits of a 16-bit integer variable. */
+#define LOW_8(x)                        ((x) & 0x00FF)
+#define HIGH_8(x)                       ((x) >> 8)
+
 /* Bit manipulation macros. */
-#define BIT_SET(a,b) ((a) |= (1 << (b)))
-#define BIT_CLEAR(a,b) ((a) &= !(1 << (b)))
-#define BIT_GET(a,b) ((a) & (1 << (b)) ? 1 : 0) 
-#define BIT_IN_BYTES(a) ((a) % 8 != 0 ? (a) / 8 + 1 : (a) / 8)
+#define BIT_SET(a,b)                    ((a) |= (1 << (b)))
+#define BIT_CLEAR(a,b)                  ((a) &= !(1 << (b)))
+#define BIT_GET(a,b)                    ((a) & (1 << (b)) ? 1 : 0) 
+#define BIT_IN_BYTES(a)                 ((a) % 8 != 0 ? (a) / 8 + 1 : (a) / 8)
 
 /* Get the numeric value for bit x. */
-#define BIT_VALUE(x) (1 << (x))
+#define BIT_VALUE(x)                    (1 << (x))
 
 #endif /* !__STORM_IA32_BIT_H__ */
