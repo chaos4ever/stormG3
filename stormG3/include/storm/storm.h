5e86665d38821869ad434d31a0dda1bc7a78981b
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-20 22:41:49 +0000

    Added dma_register, service_register, service_unregister and service_resolve to the file.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@333 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/storm.h
+++ stormG3/include/storm/storm.h
@@ -1,4 +1,4 @@
-/* $chaos: storm.h,v 1.5 2002/06/18 21:44:38 per Exp $ */
+/* $chaos: storm.h,v 1.6 2002/06/19 07:29:26 per Exp $ */
 /* Abstract: storm kernel API. This header file can be included by a
    kernel module that wants to have access to kernel functions. */
 /* Author: Per Lundberg <per@chaosdev.org> */
@@ -19,15 +19,27 @@
 /* Type definitions. */
 typedef void (irq_handler_t)(unsigned int irq_level);
 
+/* Function prototypes. */
 /* Print a debug message. */
 extern void debug_print (const char *format_string, ...);
+
+/* Register the given DMA channel. */
+extern return_t dma_register (unsigned int dma_channel, void **dma_buffer);
 
 /* Register an IRQ for use by a module. */
 extern return_t irq_register (unsigned int irq_number, char *description,
                               irq_handler_t *function);
 
-/* Register the given DMA channel. */
-extern return_t dma_register (unsigned int dma_channel, void **dma_buffer);
+/* Register a service provider. */
+extern return_t service_register (char *service, unsigned int version,
+                                  function_t handler);
+
+/* Unregister a service provider. */
+extern return_t service_unregister (char *service, function_t handler);
+
+/* Resolve a service. FIXME: Support multiple service providers. */
+extern return_t service_resolve (char *name, unsigned int version, 
+                                 function_t *handler);
 
 /* Sleep for the given amount of milliseconds. */
 extern return_t timer_sleep_milli (unsigned int time);
