c6b9f6efcdef9a9e5001b548539293804040f72e
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-23 20:33:44 +0000

    Changed to the chaos way of doing library headers.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@363 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/log/log.h
+++ libraries/log/log.h
@@ -1,4 +1,4 @@
-/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* $chaos: log.h,v 1.1 2002/06/21 08:00:47 per Exp $ */
 /* Abstract: Definition of the log protocol. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -9,43 +9,9 @@
 #define __LOG_LOG_H__
 
 #include <storm/storm.h>
-
-// FIXME: Have this be unique.
-#define LOG_COOKIE                      0x12345678
-
-enum
-{
-    /* Emergency messages are to be printed when the program has
-       encountered a serious error which forces termination. */
-    LOG_URGENCY_EMERGENCY,
-    
-    /* Something is wrong, but execution may continue. */
-    LOG_URGENCY_ERROR,
-    
-    /* Something might be wrong, but we are not entirely sure. */
-    LOG_URGENCY_WARNING,
-    
-    /* Typical informative messages are showing which hardware we
-       detected, etc. */
-    LOG_URGENCY_INFORMATIVE,
-    
-    /* Debug messages are for debugging purposes only -- normally, they
-       can be safely ignored. Eventually, they will be removed. */
-    LOG_URGENCY_DEBUG
-};
-
-/* Different function pointer types. */
-typedef return_t (*log_print_t)(unsigned int, char *);
-typedef return_t (*log_init_t)(void *);
-
-/* A log service. */
-typedef struct
-{
-    /* The log magic cookie. */
-    unsigned int magic_cookie;
-
-    /* The function log_print. */
-    log_print_t print;
-} log_service_t;
+#include <log/defines.h>
+#include <log/return_values.h>
+#include <log/types.h>
+#include <log/functions.h>
 
 #endif /* !__LOG_LOG_H__ */
