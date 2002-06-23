a0158e0d05b807867b7fbe01d463fb1be86244ac
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-23 20:34:25 +0000

    Changed to BSD license.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@365 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/unicode/unicode.h
+++ libraries/unicode/unicode.h
@@ -1,28 +1,16 @@
-/* $chaos: unicode.h,v 1.3 2002/05/23 11:14:39 per Exp $ */
+/* $chaos: unicode.h,v 1.1 2002/06/22 22:45:39 per Exp $ */
 /* Abstract: Prototypes and type definitions for the Unicode
    library. */
 /* Authors: Anders Ohrt <doa@chaosdev.org> */
 
-/* Copyright 2000 chaos development */
-
-/* This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Lesser General Public License
-   as published by the Free Software Foundation; either version 2 of
-   the License, or (at your option) any later version.
-
-   This library is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Lesser General Public License for more details.
-
-   You should have received a copy of the GNU Lesser General Public
-   License along with this library; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-   USA. */
+/* Copyright 2000-2002 chaos development */
+/* Use freely under the terms listed in the file COPYING. */
 
 #ifndef __LIBRARY_UNICODE_UNICODE_H__
 #define __LIBRARY_UNICODE_UNICODE_H__
 
+#include <storm/storm.h>
+#include <string/string.h>
 #include <unicode/return_values.h>
 #include <unicode/types.h>
 #include <unicode/functions.h>
