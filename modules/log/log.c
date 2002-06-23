568990e6659e0312d31eb13da5db886da2b943c0
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-23 20:39:44 +0000

    New define: LOG_SERVICE_VERSION New functions: log_print, log_print_formatted
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@374 1f819a86-672b-0410-b5ed-9de735de380c

--- modules/log/log.c
+++ modules/log/log.c
@@ -1,4 +1,4 @@
-/* $chaos: log.c,v 1.2 2002/06/21 08:00:08 per Exp $ */
+/* $chaos: log.c,v 1.3 2002/06/22 22:54:58 per Exp $ */
 /* Abstract: Kernel log module. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -7,6 +7,9 @@
 
 #include <storm/storm.h>
 #include <log/log.h>
+
+/* The version of log service that we provide. */
+#define LOG_SERVICE_VERSION             1
 
 static char *log_urgency[] =
 {
@@ -18,9 +21,16 @@ static char *log_urgency[] =
 };
 
 /* Print something to the kernel log. */
-static return_t log_print (unsigned int urgency __attribute__ ((unused)),
-                           char *string)
+static return_t log_print (unsigned int urgency, char *string)
 {
+    debug_print ("%s: %s\n", log_urgency[urgency], string);
+    return STORM_RETURN_SUCCESS;
+}
+
+/* Print something to the kernel log, in a more formatted fashion. */
+static return_t log_print_formatted (unsigned int urgency, char *string, ...)
+{
+    // FIXME: Use the varargs as well.
     debug_print ("%s: %s\n", log_urgency[urgency], string);
     return STORM_RETURN_SUCCESS;
 }
@@ -31,10 +41,11 @@ static return_t service_info (log_service_t *log)
 {
     log->magic_cookie = LOG_COOKIE;
     log->print = &log_print;
+    log->print_formatted = &log_print_formatted;
     return STORM_RETURN_SUCCESS;
 }
 
 return_t module_start (void)
 {
-    return service_register ("log", 1, (function_t) &service_info);
+    return service_register ("log", LOG_SERVICE_VERSION, (function_t) &service_info);
 }
