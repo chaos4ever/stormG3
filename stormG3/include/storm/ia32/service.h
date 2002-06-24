9448418cb9ac40deb258ceb16f365ea4fd148121
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-06-24 21:10:02 +0000

    Added support for multiple service providers.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@384 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/service.h
+++ stormG3/include/storm/ia32/service.h
@@ -1,4 +1,4 @@
-/* $chaos: service.h,v 1.1 2002/06/20 22:41:00 per Exp $ */
+/* $chaos: service.h,v 1.2 2002/06/24 08:32:23 per Exp $ */
 /* Abstract: Service support. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -8,16 +8,16 @@
 #ifndef __STORM_IA32_SERVICE_H__
 #define __STORM_IA32_SERVICE_H__
 
-#include <storm/storm.h>
 #include <storm/ia32/types.h>
 
 /* Definitions. */
 #define SERVICE_MAX_NAME_LENGTH         32
 #define SERVICE_MAX_VENDOR_LENGTH       32
 #define SERVICE_MAX_MODEL_LENGTH        32
-#define SERVICE_MAX_ID_LENGTH            16
+#define SERVICE_MAX_ID_LENGTH           16
 
 /* Type definitions. */
+/* This one is used internally in the kernel. */
 typedef struct
 {
     /* The service name. */
@@ -42,7 +42,46 @@ typedef struct
     function_t handler;
 
     /* A pointer to the next service. */
-    struct service_t *next;
+    struct service_data_t *next;
+} service_data_t;
+
+/* This one is used for returning information to the caller (kernel
+   module or user process alike). */
+typedef struct
+{
+    /* The service name. */
+    char name[SERVICE_MAX_NAME_LENGTH];
+
+    /* The vendor name. */
+    char vendor[SERVICE_MAX_VENDOR_LENGTH];
+
+    /* The model name. */
+    char model[SERVICE_MAX_MODEL_LENGTH];
+
+    /* A unique ID for this device. (MAC address, serial number, etc)
+       Used to distinguish between different devices with the same
+       model. */
+    char id[SERVICE_MAX_ID_LENGTH];
+
+    /* The version of the service provided. */
+    unsigned int version;
+
+    /* A function that the caller use to get information about this
+       service provider. */
+    function_t handler;
 } service_t;
+
+/* Register a service provider. */
+extern return_t service_register (char *name, char *vendor, char *model, 
+                                  char *id, unsigned int version,
+                                  function_t handler);
+
+/* Unregister a service provider. */
+extern return_t service_unregister (char *service, function_t handler);
+
+/* Resolve a service. */
+extern return_t service_resolve (char *name, char *vendor, char *model,
+                                 char *id, unsigned int version, 
+                                 size_t *services, service_t **service);
 
 #endif /* !__STORM_IA32_SERVICE_H__ */
--- stormG3/source/ia32/service.c
+++ stormG3/source/ia32/service.c
@@ -1,4 +1,4 @@
-/* $chaos: service.c,v 1.3 2002/06/24 08:13:02 hal Exp $ */
+/* $chaos: service.c,v 1.4 2002/06/24 08:32:24 per Exp $ */
 /* Abstract: Service support. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -7,21 +7,25 @@
 
 #include <storm/return_value.h>
 #include <storm/ia32/defines.h>
+#include <storm/ia32/lock.h>
 #include <storm/ia32/memory_global.h>
 #include <storm/ia32/service.h>
 #include <storm/ia32/string.h>
 
 /* Services are stored in a linked list for now. */
-static service_t *first_service = NULL;
+static service_data_t *first_service = NULL;
+
+/* Lock used to protect the service data structure. */
+lock_t service_lock = LOCK_UNLOCKED;
 
 /* Register a service provider. */
 return_t service_register (char *name, char *vendor, char *model, char *id,
                            unsigned int version,
                            function_t handler)
 {
-    service_t *service;
+    service_data_t *service;
 
-    return_t return_value = memory_global_allocate ((void **) &service, sizeof (service_t));
+    return_t return_value = memory_global_allocate ((void **) &service, sizeof (service_data_t));
     if (return_value != STORM_RETURN_SUCCESS)
     {
         return return_value;
@@ -43,9 +47,10 @@ return_t service_register (char *name, char *vendor, char *model, char *id,
     service->version = version;
     service->handler = handler;
 
-    // FIXME: Use a lock here.
-    service->next = (struct service_t *) first_service;
+    lock (&service_lock);
+    service->next = (struct service_data_t *) first_service;
     first_service = service;
+    unlock (&service_lock);
     return STORM_RETURN_SUCCESS;
 }
 
@@ -57,27 +62,75 @@ return_t service_unregister (char *service __attribute__ ((unused)),
     return STORM_RETURN_SUCCESS;
 }
 
-/* Resolve a service. FIXME: Support multiple service providers. */
+/* Resolve a service. */
 return_t service_resolve (char *name, char *vendor, char *model, char *id,
-                          unsigned int version, 
-                          function_t *handler)
+                          unsigned int version, size_t *services,
+                          service_t **out_service)
 {
-    service_t *service = first_service;
+    service_data_t *service;
+    unsigned int counter = 0;
 
+    lock (&service_lock);
+
+    (*services) = 0;
+    
+    /* Iterate two times. The first time is used to calculate the
+       number of matching services. */
+    service = first_service;
     while (service != NULL)
     {
         if ((string_compare (service->name, name) == 0 && 
-            service->version == version) &&
+             service->version == version) &&
             (vendor == NULL || string_compare (service->vendor, vendor) == 0) &&
             (model == NULL || string_compare (service->model, model) == 0) &&
             (id == NULL || string_compare (service->id, id) == 0))
         {
-            *handler = service->handler;
-            return STORM_RETURN_SUCCESS;
+            (*services)++;
         }
 
-        service = (service_t *) service->next;
+        service = (service_data_t *) service->next;
     }
 
-    return STORM_RETURN_NOT_FOUND;
+    /* Any matches? */
+    if ((*services) == 0)
+    {
+        unlock (&service_lock);
+        return STORM_RETURN_NOT_FOUND;
+    }
+
+    /* Allocate memory for the data structure. We can only allocate
+       max 4 KiB this way, but since one service is less than 128
+       byte, that's at least 32 service providers anyway. Bill Gates,
+       I love you! :-) We should use memory_virtual_allocate
+       anyway.. (at least for proceses) */
+    if (memory_global_allocate ((void **) out_service, (*services) * sizeof (service_t)) != STORM_RETURN_SUCCESS)
+    {
+        unlock (&service_lock);
+        return STORM_RETURN_OUT_OF_MEMORY;
+    }
+
+    /* Start doing our business. */
+    service = first_service;
+    while (service != NULL)
+    {
+        if ((string_compare (service->name, name) == 0 && 
+             service->version == version) &&
+            (vendor == NULL || string_compare (service->vendor, vendor) == 0) &&
+            (model == NULL || string_compare (service->model, model) == 0) &&
+            (id == NULL || string_compare (service->id, id) == 0))
+        {
+            string_copy ((*out_service)[counter].name, service->name);
+            string_copy ((*out_service)[counter].vendor, service->vendor);
+            string_copy ((*out_service)[counter].model, service->model);
+            string_copy ((*out_service)[counter].id, service->id);
+            (*out_service)[counter].version = service->version;
+            (*out_service)[counter].handler = service->handler;
+            counter++;
+        }
+
+        service = (service_data_t *) service->next;
+    }
+
+    unlock (&service_lock);
+    return STORM_RETURN_SUCCESS;
 }
