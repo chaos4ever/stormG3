fc4bbc67b62e4e016384832d19e479ad4b3e0864
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-06 09:09:35 +0000

    Moved the initial_ramdisk to a new subdirectory, where we will put all block service providers (floppy disk, IDE, SCSI, etc)
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@409 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ modules/block/initial_ramdisk/ramdisk.h
@@ -0,0 +1,29 @@
+/* $chaos: ramdisk.h,v 1.3 2002/07/06 07:55:16 per Exp $ */
+/* Abstract: Ramdisk header file. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2000-2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __RAMDISK_H__
+#define __RAMDISK_H__
+
+/* This file gets included from ramdisk.S so we need to take some
+   special precautions. */
+#ifndef ASSEMBLER
+
+extern const uint8_t ramdisk[];
+
+/* The number of blocks in this ramdisk. */
+extern unsigned int ramdisk_block_count;
+
+#endif
+
+/* The block size of the ramdisk. */
+#define RAMDISK_BLOCK_SIZE              512
+
+/* The version of the block protocol we are implement. FIXME: move to
+   the block library. */
+#define BLOCK_VERSION                   1
+
+#endif /* !__RAMDISK_H__ */
