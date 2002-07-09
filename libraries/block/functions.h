03b6c1824fd29c88fe91c80c5cad84ef694c9b5b
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-09 08:39:44 +0000

    New library.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@419 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ libraries/block/functions.h
@@ -0,0 +1,14 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: Function definitions for the block library. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __BLOCK_FUNCTIONS_H__
+#define __BLOCK_FUNCTIONS_H__
+
+/* Resolve the first block service provider. */
+extern return_t block_resolve (block_service_t *block);
+
+#endif /* !__BLOCK_FUNCTIONS_H__ */
