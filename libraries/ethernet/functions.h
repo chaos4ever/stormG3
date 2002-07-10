79b5cbaccb443a8fabc25b89183188e11f94447a
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-10 20:11:02 +0000

    Base-files for the ethernet-library. Will be changed.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@425 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ libraries/ethernet/functions.h
@@ -0,0 +1,12 @@
+/* $chaos */
+/* Abstract: Ethernet library functions. */
+/* Author: Johannes Lundberg <jojo@chaosdev.org>
+           Håkan Larsson     <trc@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __ETHERNET_FUNCTIONS_H__
+#define __ETHERNET_FUNCTIONS_H__
+
+#endif /* !__ETHERNET_FUNCTIONS_H__ */
