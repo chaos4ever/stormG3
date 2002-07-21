190cc04fc59263273bce17c818bc7f907f20ab30
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-21 13:02:18 +0000

    Renamed find_inode2 to minix2_find_inode New function: minix_init New function: minix2_find_file New function: service_info Added registration of a filesystem service.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@482 1f819a86-672b-0410-b5ed-9de735de380c

--- modules/filesystem/minix/minix.c
+++ modules/filesystem/minix/minix.c
@@ -1,4 +1,4 @@
-/* $chaos: minix.c,v 1.1 2002/07/09 08:37:37 per Exp $ */
+/* $chaos: minix.c,v 1.2 2002/07/10 21:23:33 per Exp $ */
 /* Abstract: Implementation of the Minix file system. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -7,7 +7,9 @@
 
 #include <storm/storm.h>
 #include <block/block.h>
+#include <filesystem/filesystem.h>
 #include <log/log.h>
+#include <string/string.h>
 
 #include "minix.h"
 
@@ -24,7 +26,7 @@ uint8_t minix_buffer[MINIX_BLOCK_SIZE];
 uint8_t minix_buffer2[MINIX_BLOCK_SIZE];
 
 /* Find an inode with the given inode number. */
-static minix2_inode_t *find_inode2 (minix_fs_t *minix_fs, unsigned int number)
+static minix2_inode_t *minix2_find_inode (minix_fs_t *minix_fs, unsigned int number)
 {
     unsigned int inode_block = minix_fs->inode_block + (number - 1) / MINIX2_INODE_PER_BLOCK;
     unsigned int inode_which = (number - 1) % MINIX2_INODE_PER_BLOCK;
@@ -33,27 +35,11 @@ static minix2_inode_t *find_inode2 (minix_fs_t *minix_fs, unsigned int number)
     return (minix2_inode_t *) (&minix_buffer2[inode_which * sizeof (minix2_inode_t)]);
 }
 
-int module_start (void)
+/* Make sure this is a Minix file system and set up some structures so
+   we can access it. Return TRUE if it is a valid, supported file
+   system and FALSE otherwise. */
+static bool minix_init (minix_fs_t *minix_fs)
 {
-    minix_fs_t *minix_fs = &fs;
-    block_info_t info;
-
-    /* Contact the log service provider. */
-    if (log_init (&log) != LOG_RETURN_SUCCESS)
-    {
-        return STORM_RETURN_NOT_FOUND;
-    }
-
-    if (block_resolve (&minix_fs->block) != BLOCK_RETURN_SUCCESS)
-    {
-        log.print (LOG_URGENCY_EMERGENCY,
-                   "No block device found.");
-        return -1;
-    }
-
-    minix_fs->block.info(&info);
-    minix_fs->block_size = info.block_size;
-
     /* Make sure this is a valid Minix file system. */
     if (minix_fs->block.read (MINIX_SUPERBLOCK_LOCATION / minix_fs->block_size, 
                               MINIX_BLOCK_SIZE / minix_fs->block_size,
@@ -62,50 +48,57 @@ int module_start (void)
         if (superblock->magic == MINIX_MAGIC)
         {
             minix_fs->version = 1;
-            minix_fs->name_length = 14;
+            minix_fs->name_length = MINIX_FILE_NAME_LENGTH;
         }
         else if (superblock->magic == MINIX_MAGIC_EXTENDED)
         {
             minix_fs->version = 1;
-            minix_fs->name_length = 30;
+            minix_fs->name_length = MINIX_EXTENDED_FILE_NAME_LENGTH;
         }
         else if (superblock->magic == MINIX2_MAGIC)
         {
             minix_fs->version = 2;
-            minix_fs->name_length = 14;
+            minix_fs->name_length = MINIX_FILE_NAME_LENGTH;
         }
         else if (superblock->magic == MINIX2_MAGIC_EXTENDED)
         {
             minix_fs->version = 2;
-            minix_fs->name_length = 30;
+            minix_fs->name_length = MINIX_EXTENDED_FILE_NAME_LENGTH;
         }
         else
         {
             debug_print ("Not a valid file system.\n");
-            return -1;
+            return FALSE;
         }
 
-        debug_print ("Minix file system version %d found.\n", minix_fs->version);
+        //        debug_print ("Minix file system version %d found.\n", minix_fs->version);
 
         if (superblock->state == 1)
         {
-            debug_print ("Filesystem is clean.\n");
+            //            debug_print ("Filesystem is clean.\n");
         }
         else
         {
             debug_print ("File system has errors.\n");
-            return -1;
+            return FALSE;
         }
     }
     else
     {
         debug_print ("Reading from block service failed.\n");
+        return FALSE;
     }
 
+    /* Locate the inode block. */
     minix_fs->inode_block = (2 + superblock->imap_blocks +
                              superblock->zmap_blocks);
+    return TRUE;
+}
 
-    /* Okay, it is a file system. Now, read the first inode. */
+/* Find a file in the root directory of a Minix v2 filesystem. */
+static minix2_inode_t *minix2_find_file (minix_fs_t *minix_fs, char *filename)
+{
+    /* Read the root inode. */
     if (minix_fs->block.read ((minix_fs->inode_block * MINIX_BLOCK_SIZE) / minix_fs->block_size, MINIX_BLOCK_SIZE / minix_fs->block_size, minix_buffer) == 0)
     {
         minix_inode_t *inode = (minix_inode_t *) &minix_buffer;
@@ -116,7 +109,7 @@ int module_start (void)
             if ((inode->mode & MINIX_MODE_DIRECTORY) != MINIX_MODE_DIRECTORY)
             {
                 debug_print ("Root inode is not a directory.\n");
-                return -1;
+                return NULL;
             }
         }
         else if (minix_fs->version == 2)
@@ -126,7 +119,7 @@ int module_start (void)
             if ((inode2->mode & MINIX_MODE_DIRECTORY) != MINIX_MODE_DIRECTORY)
             {
                 debug_print ("v2: Root inode is not a directory.\n");
-                return -1;
+                return NULL;
             }
 
             /* Okay, so the root node is a directory. Fine and
@@ -139,9 +132,11 @@ int module_start (void)
                 while (location < directory_size)
                 {
                     minix_directory_entry_t *directory_entry = (minix_directory_entry_t *) &minix_buffer[location];
-                    inode2 = find_inode2 (minix_fs, directory_entry->inode);
-                    debug_print ("%s %x %u %u\n", directory_entry->name,
-                                 inode2->mode, inode2->uid, inode2->gid);
+                    if (string_compare_max(filename, directory_entry->name, minix_fs->name_length) == 0)
+                    {
+                        return minix2_find_inode (minix_fs, directory_entry->inode);
+                    }
+
                     location += sizeof (minix_directory_entry_t) + minix_fs->name_length;
                 }
                 
@@ -156,6 +151,55 @@ int module_start (void)
     {
         debug_print ("Reading root inode failed.\n");
     }
-    
-    return 0;
+
+    return NULL;
+}
+
+/* Return some information about the filesystem service (function pointers to
+   our functionality). */
+static return_t service_info (void *filesystem_void)
+{
+    filesystem_service_t *filesystem = (filesystem_service_t *) filesystem_void;
+    filesystem->magic_cookie = FILESYSTEM_COOKIE;
+    // filesystem->mount = &minix_mount;
+    // filesystem->read = &minix_read;
+    return STORM_RETURN_SUCCESS;
+}
+
+int module_start (void)
+{
+    minix_fs_t *minix_fs = &fs;
+    block_info_t info;
+    //    minix2_inode_t *inode2;
+
+    /* Contact the log service provider. */
+    if (log_init (&log) != LOG_RETURN_SUCCESS)
+    {
+        return STORM_RETURN_NOT_FOUND;
+    }
+
+    /* Resolve a block device. FIXME: Remove this code and replace it
+       with VFS mounting. */
+    if (block_resolve (&minix_fs->block) != BLOCK_RETURN_SUCCESS)
+    {
+        log.print (LOG_URGENCY_EMERGENCY,
+                   "No block device found.");
+        return -1;
+    }
+
+    minix_fs->block.info(&info);
+    minix_fs->block_size = info.block_size;
+
+    if (!minix_init (minix_fs))
+    {
+        log.print (LOG_URGENCY_EMERGENCY, "Not a valid Minix file system.");
+        return -1;
+    }
+
+    //    inode2 = minix2_find_file (minix_fs, "razor_1911_cracks.mod");
+    //    debug_print ("%s %x %u %u\n", "razor_1911_cracks.mod",
+    //                 inode2->mode, inode2->uid, inode2->gid);
+
+    return service_register ("filesystem", "chaos development", "Minix module",
+                             "1", FILESYSTEM_SERVICE_VERSION, &service_info);
 }
