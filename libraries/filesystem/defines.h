d1af3e4c2960ab06e3f986c650125cf89edd33a6
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-21 12:52:13 +0000

    Added a lost file.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@476 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ libraries/filesystem/defines.h
@@ -0,0 +1,18 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: Defines used by the filesystem library. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __FILESYSTEM_DEFINES_H__
+#define __FILESYSTEM_DEFINES_H__
+
+/* Defines. */
+// FIXME: This should be unique.
+#define FILESYSTEM_COOKIE               0x12345678
+
+/* The version of the filesystem service that we are using. */
+#define FILESYSTEM_SERVICE_VERSION      1
+
+#endif /* !__FILESYSTEM_DEFINES_H__ */
