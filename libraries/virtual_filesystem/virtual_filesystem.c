0af1a3dad5b2132658f266ec117e7115f28953c7
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-21 12:57:57 +0000

    Changed the file to actually use the virtual filesystem provider. :)
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@480 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/virtual_filesystem/virtual_filesystem.c
+++ libraries/virtual_filesystem/virtual_filesystem.c
@@ -1,29 +1,29 @@
-/* $chaos: filesystem.c,v 1.1 2002/07/21 12:38:40 per Exp $ */
-/* Abstract: Filesystem library. */
+/* $chaos: virtual_filesystem.c,v 1.1 2002/07/21 12:55:51 per Exp $ */
+/* Abstract: Virtual filesystem library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file COPYING. */
 
-#include <filesystem/filesystem.h>
+#include <virtual_filesystem/virtual_filesystem.h>
 
-/* Resolve the first file system service provider. FIXME: Should be
-   able to return a list. */
-return_t filesystem_resolve (filesystem_service_t *filesystem)
+/* Resolve the first virtual filesystem service provider. FIXME:
+   Should be able to return a list. */
+return_t virtual_filesystem_resolve (virtual_filesystem_service_t *virtual_filesystem)
 {
     size_t services;
     service_t *service;
 
-    /* Find the filesystem service. */
-    if (service_resolve ("block", NULL, NULL, NULL, 1, &services, &service) != STORM_RETURN_SUCCESS)
+    /* Find the virtual filesystem service. */
+    if (service_resolve ("virtual_filesystem", NULL, NULL, NULL, 1, &services, &service) != STORM_RETURN_SUCCESS)
     {
-        debug_print ("Failed to resolve block service provider.\n");
-        return FILESYSTEM_RETURN_SERVICE_UNAVAILABLE;
+        debug_print ("Failed to resolve virtual filesystem service provider.\n");
+        return VIRTUAL_FILESYSTEM_RETURN_SERVICE_UNAVAILABLE;
     }
 
-    service[0].service_info (filesystem);
+    service[0].service_info (virtual_filesystem);
 
     // FIXME: Free the memory allocated by service_resolve.
 
-    return FILESYSTEM_RETURN_SUCCESS;
+    return VIRTUAL_FILESYSTEM_RETURN_SUCCESS;
 }
