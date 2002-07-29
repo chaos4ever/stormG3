f7b46ee955e4a31c6c23f8be14005d1744bd7f4a
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-07-29 07:11:20 +0000

    New type: filesystem_mount_t New member of the filesystem_service_t structure: mount
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@505 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/filesystem/types.h
+++ libraries/filesystem/types.h
@@ -1,4 +1,4 @@
-/* $chaos: types.h,v 1.2 2002/07/06 08:29:22 per Exp $ */
+/* $chaos: types.h,v 1.1 2002/07/21 12:38:40 per Exp $ */
 /* Abstract: Types used by the filesystem library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -8,11 +8,17 @@
 #ifndef __FILESYSTEM_TYPES_H__
 #define __FILESYSTEM_TYPES_H__
 
+#include <block/block.h>
+
+typedef return_t (*filesystem_mount_t)(block_service_t *block);
+
 typedef struct
 {
     /* The block magic cookie. */
     unsigned int magic_cookie;
 
+    /* Mount a volume. */
+    filesystem_mount_t mount;
 } filesystem_service_t;
 
 #endif /* !__FILESYSTEM_TYPES_H__ */
