cd1d5478844b52bbdda7b04bbe34d6c130b33dcd
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-08-04 09:28:24 +0000

    Improved the filesystem_service_t with more function pointers.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@515 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/filesystem/types.h
+++ libraries/filesystem/types.h
@@ -1,4 +1,4 @@
-/* $chaos: types.h,v 1.1 2002/07/21 12:38:40 per Exp $ */
+/* $chaos: types.h,v 1.2 2002/07/29 07:11:20 per Exp $ */
 /* Abstract: Types used by the filesystem library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -9,8 +9,12 @@
 #define __FILESYSTEM_TYPES_H__
 
 #include <block/block.h>
+#include <vfs/vfs.h>
 
 typedef return_t (*filesystem_mount_t)(block_service_t *block);
+typedef return_t (*filesystem_open_t)(char *filename, vfs_file_mode_t mode, vfs_file_handle_t *handle);
+typedef return_t (*filesystem_close_t)(vfs_file_handle_t handle);
+typedef return_t (*filesystem_read_t)(vfs_file_handle_t handle, void *buffer, size_t count);
 
 typedef struct
 {
@@ -19,6 +23,18 @@ typedef struct
 
     /* Mount a volume. */
     filesystem_mount_t mount;
+
+    /* Open a file. */
+    filesystem_open_t open;
+
+    /* Close a file. */
+    filesystem_close_t close;
+
+    /* Read from a file. */
+    filesystem_read_t read;
+
+    // TODO:
+    /* Write to a file. */
 } filesystem_service_t;
 
 #endif /* !__FILESYSTEM_TYPES_H__ */
