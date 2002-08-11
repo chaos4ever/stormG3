9854d502ff878bcc1a88d6edb4db17cef05a1312
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-08-11 21:13:32 +0000

    Moved from keyboard to system/keyboard.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@578 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ modules/system/keyboard/init.h
@@ -0,0 +1,13 @@
+/* $chaos: init.h,v 1.1 2002/06/17 22:53:47 per Exp $ */
+/* Abstract: Initialization functions. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2000-2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __INIT_H__
+#define __INIT_H__
+
+extern bool init (void);
+
+#endif /* !__INIT_H__ */
