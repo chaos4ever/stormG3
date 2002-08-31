957e597ef0807be76226a8ab9c390dbee44bbc01
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-08-31 09:17:40 +0000

    Ported the checksum library from the old chaos (based on non-LGPL code originally, so no license problem).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@618 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ libraries/checksum/defines.h
@@ -0,0 +1,14 @@
+/* $chaos: defines.h,v 1.3 2002/05/23 11:14:25 per Exp $ */
+/* Abstract: Defines used by the checksum library. */
+/* Author: Per Lundberg <plundis@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#ifndef __LIBRARY_CHECKSUM_DEFINES_H__
+#define __LIBRARY_CHECKSUM_DEFINES_H__
+
+/* Defines. */
+#define CHECKSUM_MD5_DIGEST_LENGTH 16
+
+#endif /* !__LIBRARY_CHECKSUM_DEFINES_H__ */
