beb9ca79bc89c8f772cc9071dc27b1701e58cfd9
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-09-01 21:02:22 +0000

    Added an embry of an ELF module, providing the exec service. This is the first step towards user processes in chaosG2.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@622 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ modules/system/elf/elf.c
@@ -0,0 +1,13 @@
+/* $chaos: xemacs-script,v 1.5 2002/05/23 11:22:14 per Exp $ */
+/* Abstract: ELF module, implementing the exec service. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file COPYING. */
+
+#include <storm/storm.h>
+
+return_t module_start (void)
+{
+    return STORM_RETURN_SUCCESS;
+}
