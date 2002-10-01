0a3bd6f8d695484a709bd19defd1bdefa99c09bc
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-01 19:37:32 +0000

    Started the real coding.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@626 1f819a86-672b-0410-b5ed-9de735de380c

--- modules/ethernet/3c509/3c509.c
+++ modules/ethernet/3c509/3c509.c
@@ -1,4 +1,4 @@
-/* $chaos: 3c509.c,v 1.4 2002/07/12 09:19:29 johannes Exp $ */
+/* $chaos: 3c509.c,v 1.5 2002/07/12 10:13:31 johannes Exp $ */
 /* Abstract: 3Com Etherlink III module (3c509) */
 /* Author: Johannes Lundberg <jojo@chaosdev.org>
            Håkan Larsson     <trc@chaosdev.org> */
@@ -17,24 +17,26 @@
 
 log_service_t log;
 
-static return_t ethernet_send (ethernet_packet_t *packet unused, int length unused)
+static return_t ethernet_send
+  (ethernet_packet_t *packet unused, int length unused)
 {
   return ETHERNET_RETURN_OPERATION_NOT_SUPPORTED;
 }
 
-static return_t ethernet_send_raw (ethernet_packet_t *packet unused, int length unused)
+static return_t ethernet_send_raw
+  (ethernet_packet_t *packet unused, int length unused)
 {
   return ETHERNET_RETURN_OPERATION_NOT_SUPPORTED;
 }
 
 static return_t ethernet_protocol_register
-                  (int protocol unused, ethernet_receiver_t ethernet_receiver unused)
+  (int protocol unused, ethernet_receiver_t ethernet_receiver unused)
 {
   return ETHERNET_RETURN_OPERATION_NOT_SUPPORTED;
 }
 
 static return_t ethernet_protocol_unregister
-                  (int protocol unused, ethernet_receiver_t ethernet_receiver unused)
+  (int protocol unused, ethernet_receiver_t ethernet_receiver unused)
 {
   return ETHERNET_RETURN_OPERATION_NOT_SUPPORTED;
 }
@@ -51,7 +53,8 @@ static return_t ethernet_info_set (ethernet_info_t ethernet_info unused)
 
 static return_t service_info (void *ethernet_void)
 {
-  ethernet_service_t *ethernet_service = (ethernet_service_t *) ethernet_void;
+  ethernet_service_t *ethernet_service =
+    (ethernet_service_t *) ethernet_void;
 
   ethernet_service->magic_cookie        = ETHERNET_COOKIE;
   ethernet_service->send                = &ethernet_send;
@@ -64,28 +67,45 @@ static return_t service_info (void *ethernet_void)
 
 return_t probe (void)
 {
-  int id_port;
+  unsigned int id_port;
+  unsigned int counter;
+  unsigned int al;
 
-  /* Select an open I/O location at 0x1*0, and probe for the 3c509 card. */
+  /* Select an open I/O location at 0x1*0. */
 
   for (id_port = 0x110; id_port < 0x200; id_port += 0x10)
   {
-    if (port_range_register (id_port, 1 , "3Com Etherlink III ID port") !=
+    if (port_range_register (id_port, 1, "3Com Etherlink III ID port") !=
         STORM_RETURN_SUCCES)
     {
       continue;
     }
-    port_out (id_port, 0x00);
-    port_out (id_port, 0xFF);
-    if (port_in_u8 (id_port) & 0x01)
-    {
-      break;
-    }
-  } 
+  }
 
-  if (id_port >= 0x200)
+  if (id_port == 0x200)
   {
+    log.print (LOG_URGENCY_EMERGENCY,
+               "No free I/O-port found. Can't activate card.\n");
+    return;
+  }
+
+  /* Active ID port. */
+  
+  port_uint8_out (id_port, 0x00);
     
+  /* Generate and write the ID sequence (Chapter 7, page 2 in the PDF) */
+
+  al = 0xFF;
+  for (counter = 0xFF; counter > 0; counter --)
+  {
+    port_uint8_out (id_port, al);
+
+    al = al << 1;
+    if (al & 0x100)
+    {
+      al = al ^ 0xCF;
+      al = al & 0x00FF;
+    }
   }
 } 
 
