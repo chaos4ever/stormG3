73317a652356c0b0477a94c620e53c71d3954446
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-04 21:23:19 +0000

    New define: EXEC_SERVICE_VERSION
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@637 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/exec/defines.h
+++ libraries/exec/defines.h
@@ -1,4 +1,4 @@
-/* $chaos: dotfile.emacs,v 1.34 2002/09/30 13:33:00 per Exp $ */
+/* $chaos: defines.h,v 1.1 2002/10/04 20:53:22 per Exp $ */
 /* Abstract: Definitions used by the exec library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -7,5 +7,8 @@
 
 #ifndef __EXEC_DEFINES_H__
 #define __EXEC_DEFINES_H__
+
+/* The version of the block service. */
+#define EXEC_SERVICE_VERSION           1
 
 #endif /* !__EXEC_DEFINES_H__ */
