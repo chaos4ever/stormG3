86339524374717339a3fc7e256296c8ef59ddbd4
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-04 21:23:56 +0000

    New prototype: exec_register
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@639 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/exec/functions.h
+++ libraries/exec/functions.h
@@ -1,4 +1,4 @@
-/* $chaos: dotfile.emacs,v 1.34 2002/09/30 13:33:00 per Exp $ */
+/* $chaos: functions.h,v 1.1 2002/10/04 20:53:22 per Exp $ */
 /* Abstract: Functions in the exec library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -10,5 +10,9 @@
 
 /* Lookup the first exec service provider. */
 extern return_t exec_lookup (exec_service_t *exec);
+
+/* Register an exec service provider. */
+extern return_t exec_register (char *vendor, char *model, char *id,
+                               service_info_t service_info);
 
 #endif /* !__EXEC_FUNCTIONS_H__ */
