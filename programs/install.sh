2e024e9a3126f1019276ccdd433c4b400d07cc26
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-06 09:43:21 +0000

    New files.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@648 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ programs/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $Id$
+# Abstract: Install our programs onto the ramdisk.
+# Author: Per Lundberg <per@chaosdev.org>
+
+export PROGRAMS=boot
+export ROOT=/tftpboot
+export RAMDISK_ROOT=/mnt/chaos-ramdisk
+
+# The mount/umount is neccessary because otherwise the filesystem will
+# be marked as dirty -- and we don't mount a dirty filesystem.
+
+mount ${RAMDISK_ROOT}
+for e in ${PROGRAMS}
+do
+  echo "Copying $e"
+  cp ${ROOT}/system/programs/$e ${RAMDISK_ROOT} || (umount ${RAMDISK_ROOT} ; exit 1)
+done
+umount ${RAMDISK_ROOT}
