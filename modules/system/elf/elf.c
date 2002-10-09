4e7dab1460a9fad8ecdc1b4bc00f195d51907f63
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-09 08:31:25 +0000

    Use ELF_TYPE_EXECUTABLE instead of ELF_TYPE_DYNAMIC (we support programs, not shared libraries, with this function). New function: elf_load Added more code to elf_run (it should basically work when we finish elf_load).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@662 1f819a86-672b-0410-b5ed-9de735de380c

--- modules/system/elf/elf.c
+++ modules/system/elf/elf.c
@@ -1,4 +1,4 @@
-/* $chaos: elf.c,v 1.2 2002/10/04 19:01:13 per Exp $ */
+/* $chaos: elf.c,v 1.3 2002/10/04 21:26:26 per Exp $ */
 /* Abstract: ELF module, implementing the exec service. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -19,19 +19,56 @@ static return_t elf_identify (elf_header_t *elf_header)
           elf_header->identification[2] == 'L' &&
           elf_header->identification[3] == 'F'))
     {
+        debug_print ("No ELF header found.\n");
         return STORM_RETURN_MODULE_INVALID;
     }
 
     /* ...and a 32-bit one, little-endian, relocatable... */
     if (elf_header->class != ELF_CLASS_32BIT ||
         elf_header->endian != ELF_ENDIAN_LITTLE ||
-        elf_header->type != ELF_TYPE_DYNAMIC ||
+        elf_header->type != ELF_TYPE_EXECUTABLE ||
         elf_header->version != ELF_VERSION_CURRENT ||
         elf_header->machine != ELF_MACHINE_386)
     {
+        debug_print ("Invalid type of ELF detected.\n");
         return STORM_RETURN_MODULE_INVALID;
     }
 
+    return STORM_RETURN_SUCCESS;
+}
+
+/* Load an ELF. Allocate memory for it, and copy the data from the
+   different sections there. */
+static return_t elf_load (elf_header_t *elf_header, 
+                          process_id_t process_id UNUSED)
+{
+    //    return_t return_value;
+    //    void *image;
+
+    // FIXME: Rewrite this code from scratch, it's not usable in the
+    // process ELF execution.
+    /* Find the highest end address of a section. This presumes that
+       the sections come right after each other, which is a reasonable
+       presumption since we control ELF generation for our OS. */
+    for (int index = 0; index < elf_header->section_header_entries; index++)
+    {
+        elf_section_header_t *section_header = (elf_section_header_t *) (((uint32_t) elf_header) + elf_header->section_header_offset + (index * elf_header->section_header_entry_size));
+        if (section_header->flags & ELF_SECTION_FLAG_ALLOCATE)
+        {
+            // void *buffer; 
+
+            /* Allocate memory for this section and map it at the
+               right place. */
+            // memory_physical_allocate_for_process (buffer, process_id);
+            // memory_virtual_map ()
+
+            /* Copy the data from the image. */
+            //memory_copy ((void *) (((address_t) image) + section_header->address),
+            //                         (void *) (((address_t) elf_header) + section_header->offset),
+            //                         section_header->size);
+        }
+    }
+  
     return STORM_RETURN_SUCCESS;
 }
 
@@ -40,19 +77,48 @@ static return_t elf_identify (elf_header_t *elf_header)
    simple actually... :) */
 static return_t elf_run (void *buffer)
 {
+    return_t return_value;
     elf_header_t *elf_header = (elf_header_t *) buffer;
+    process_id_t process_id;
+    page_directory_t *page_directory;
 
-    /* Make sure the ELF is valid. */
-    return_t return_value = elf_identify (elf_header);
+    /* Make sure this is a valid ELF. */
+    return_value = elf_identify (elf_header);
     if (return_value != STORM_RETURN_SUCCESS)
     {
+        debug_print ("Invalid ELF.\n");
         return return_value;
     }
 
-    /* Allocate memory for the different sections. */
-    /* Copy the sections. */
-    /* Create the proces with the kernel. */
-    return STORM_RETURN_SUCCESS;
+    /* Seems to be a valid ELF! We try and load it. But first, we need
+       the kernel set up some things for us:
+
+         1) a page directory (for mapping)
+         2) a process ID (for allocating memory) 
+
+       This is done by the very nice kernel function
+       process_precreate. We have done it this way so that adding
+       support for another binary format is incredibly
+       simple. Ideally, it should not even require a reboot of the
+       system. */
+    return_value = process_precreate (&process_id, &page_directory);
+    if (return_value != STORM_RETURN_SUCCESS)
+    {
+        debug_print ("Failed precreating process.\n");
+        return return_value;
+    }
+   
+    /* Allocate pages and copy the sections to this space. */
+    return_value = elf_load (elf_header, process_id);
+    if (return_value != STORM_RETURN_SUCCESS)
+    {
+        debug_print ("Failed loading ELF.\n");
+        return return_value;
+    }
+
+    /* Create the process. */
+    return_value = process_create (process_id, elf_header->entry_point);
+    return return_value;
 }
 
 /* Return some information about the exec service. */
