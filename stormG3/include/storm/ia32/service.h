7f33549fcccd35036096f707e080d36ff7855bbf
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-09 12:50:50 +0000

    Fixed the comments.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@675 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/service.h
+++ stormG3/include/storm/ia32/service.h
@@ -1,4 +1,4 @@
-/* $chaos: service.h,v 1.6 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: service.h,v 1.7 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -21,72 +21,106 @@
 #define SERVICE_MAX_ID_LENGTH           16
 
 /* Type definitions. */
-/** \brief A function that provides info about this service provider. The data
+/** @brief A function that provides info about this service provider. The data
     returned is protocol specific. */
 typedef return_t (*service_info_t)(void *);
 
-/** \brief Information about a service provider. */
+/** @brief Information about a service provider. */
 typedef struct
 {
-    /** The service name. */
+    /**
+     * @brief The service name. 
+     */
     char name[SERVICE_MAX_NAME_LENGTH];
 
-    /** The vendor name. */
+    /**
+     * @brief The vendor name. 
+     */
     char vendor[SERVICE_MAX_VENDOR_LENGTH];
 
-    /** The model name. */
+    /**
+     * @brief The model name. 
+     */
     char model[SERVICE_MAX_MODEL_LENGTH];
 
-    /** A unique ID for this device. (MAC address, serial number, etc)
-        Used to distinguish between different devices with the same
-        model. */
+    /**
+     * @brief A unique ID for this device. (MAC address, serial
+     * number, etc) Used to distinguish between different devices with
+     * the same model. 
+     */
     char id[SERVICE_MAX_ID_LENGTH];
 
-    /** The version of the service provided. */
+    /**
+     * @brief The version of the service provided. 
+     */
     unsigned int version;
 
-    /** A function that the caller use to get information about this
-        service provider. */
+    /**
+     * @brief A function that the caller use to get information about
+     * this service provider. 
+     */
     service_info_t service_info;
 
-    /** A pointer to the next service. */
+    /**
+     * @brief A pointer to the next service. 
+     */
     struct service_data_t *next;
 } service_data_t;
 
-/** \brief A structure used for returning information to the caller. */
+/**
+ * @brief A structure used for returning information to the caller. 
+ */
 typedef struct
 {
-    /** The service name. */
+    /**
+     * @brief The service name. 
+     */
     char name[SERVICE_MAX_NAME_LENGTH];
 
-    /** The vendor name. */
+    /**
+     * @brief The vendor name. 
+     */
     char vendor[SERVICE_MAX_VENDOR_LENGTH];
 
-    /** The model name. */
+    /**
+     * @brief The model name. 
+     */
     char model[SERVICE_MAX_MODEL_LENGTH];
 
-    /** A unique ID for this device. (MAC address, serial number, etc)
-        Used to distinguish between different devices with the same
-        model. */
+    /**
+     * @brief unique ID for this device. (MAC address, serial number,
+     * etc) Used to distinguish between different devices with the
+     * same model. 
+     */
     char id[SERVICE_MAX_ID_LENGTH];
 
-    /** The version of the service provided. */
+    /**
+     * @brief The version of the service provided. 
+     */
     unsigned int version;
 
-    /** A function that the caller use to get information about this
-        service provider. */
+    /**
+     * @brief A function that the caller use to get information about this
+     * service provider. 
+     */
     service_info_t service_info;
 } service_t;
 
-/** \brief Register a service provider. */
+/**
+ * @brief Register a service provider. 
+ */
 extern return_t service_register (char *name, char *vendor, char *model, 
                                   char *id, unsigned int version,
                                   service_info_t service_info);
 
-/** \brief Unregister a service provider. */
+/**
+ * @brief Unregister a service provider. 
+ */
 extern return_t service_unregister (char *service, function_t handler);
 
-/** \brief Lookup a service. */
+/** 
+ * @brief Lookup a service. 
+ */
 extern return_t service_lookup (char *name, char *vendor, char *model,
                                 char *id, unsigned int version, 
                                 size_t *services, service_t **service);
