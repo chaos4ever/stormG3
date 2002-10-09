b760cfa8356ea1aa7beff623d176c8a85391e74d
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-09 13:07:13 +0000

    Improved the comments a bit.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@678 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/bit.h
+++ stormG3/include/storm/ia32/bit.h
@@ -1,25 +1,42 @@
-/* $chaos: bit.h,v 1.6 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: bit.h,v 1.7 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
-/** @file bit.h
-    @brief Bit manipulation macros. */
+/** 
+ * @file bit.h
+ * @brief Bit manipulation macros. 
+ *
+ * This file is endian-specific and needs to be rewritten for
+ * big-endian when we port to such a CPU. It provides very convenient
+ * functions for reading and writing bit values to a integers, as well
+ * as reading the upper/lower half of a 32-bit and 16-bit integer.
+ */
 
 #ifndef __STORM_IA32_BIT_H__
 #define __STORM_IA32_BIT_H__
 
-/* This file is endian-specific. */
 
-/* Get the low or high 16 bits of a 32-bit integer
-   variable. */
+/** 
+ * @brief Get the low 16 bits of a 32-bit integer variable. 
+ */
 #define LOW_16(x)                       ((x) & 0xFFFF)
+
+/** 
+ * @brief Get the high 16 bits of a 32-bit integer variable. 
+ */
 #define HIGH_16(x)                      ((x) >> 16)
 
-/* Get the low or high 8 bits of a 16-bit integer variable. */
+/**
+ * @brief Get the low 8 bits of a 16-bit integer variable. 
+ */
 #define LOW_8(x)                        ((x) & 0xFF)
+
+/**
+ * @brief Get the high 8 bits of a 16-bit integer variable. 
+ */
 #define HIGH_8(x)                       ((x) >> 8)
 
 /* Bit manipulation macros. */
@@ -28,7 +45,9 @@
 #define BIT_GET(a,b)                    ((a) & (1 << (b)) ? 1 : 0) 
 #define BIT_IN_BYTES(a)                 ((a) % 8 != 0 ? (a) / 8 + 1 : (a) / 8)
 
-/* Get the numeric value for bit x. */
+/**
+ * @brief Get the numeric value for bit x. 
+ */
 #define BIT_VALUE(x)                    (1 << (x))
 
 #endif /* !__STORM_IA32_BIT_H__ */
