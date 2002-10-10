b801a8c29eedcb027ca4e378df528cae296cf2ed
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 14:09:12 +0000

    Added Doxygen comments.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@703 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/gdb.h
+++ stormG3/include/storm/ia32/gdb.h
@@ -1,19 +1,39 @@
-/* $chaos: gdb.h,v 1.2 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: gdb.h,v 1.3 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /** 
- * @file gdb.h
- * @brief GDB support. 
+ * @file        gdb.h
+ * @brief       GDB support. 
  */
 
 #ifndef __STORM_IA32_GDB_H__
 #define __STORM_IA32_GDB_H__
 
+/**
+ * @brief               A breakpoint.
+ *
+ * You can add a breakpoint in the code just by running this
+ * macro. When you then load the kernel with serial debugging enabled,
+ * the debugger will break on this point.
+ */
 #define BREAKPOINT() asm("int $3");
+
+/**
+ * @brief               Initialize serial GDB debugging.
+ * @param port          The number of the serial port to use.
+ * @param speed         The speed to use.
+ */
 extern void gdb_serial_init (unsigned short port, unsigned int speed);
+
+/**
+ * @brief               Set up GDB debug traps.
+ * 
+ * These traps replace the ordinary exception handlers, and instead of
+ * showing a crash screen, they give control over to GDB.
+ */
 extern void gdb_set_debug_traps (void);
 
 #endif /* !__STORM_IA32_GDB_H__ */
