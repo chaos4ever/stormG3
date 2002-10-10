d85e61462fc7941685216cf6b7789d1a4dda8da0
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 20:36:39 +0000

    Improved the Doxygen documentation.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@718 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/idt.h
+++ stormG3/include/storm/ia32/idt.h
@@ -1,12 +1,12 @@
-/* $chaos: idt.h,v 1.4 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: idt.h,v 1.5 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file idt.h
- * @brief  Function prototypes for IDT functions. 
+ * @file                idt.h
+ * @brief               Function prototypes for IDT functions. 
  */
 
 #ifndef __STORM_IA32_IDT_H__
@@ -14,26 +14,46 @@
 
 #include <storm/ia32/descriptor.h>
 
-/* Defines. */
-/* The number of entries in the IDT. */
+/**
+ * @brief               The number of entries in the IDT. 
+ */
 #define IDT_ENTRIES                     (256)
 
+/**
+ * @brief               The size of the IDT.
+ */
 #define IDT_SIZE                        (IDT_ENTRIES * sizeof (descriptor_t))
 
-/* Function prototypes. */
-/* Create a trap gate in the IDT. */
+/**
+ * @brief               Create a trap gate in the IDT. 
+ * @param number        The number of the IDT entry.
+ * @param selector      The selector to point at.
+ * @param address       The address of the function to call as the
+ *                      exception handler.
+ * @param privilege_level
+ *                      The RPL (Requestors Privilege Level) of this gate.
+ */
 extern void idt_setup_trap_gate (unsigned int number,
                                  unsigned int selector, void *address, 
                                  unsigned int privilege_level);
 
-/* Create an interrupt gate in the IDT. */
+/**
+ * @brief Create an interrupt gate in the IDT. 
+ * @param number        The number of the IDT entry.
+ * @param selector      The selector to point at.
+ * @param address       The address of the function to call as the
+ *                      exception handler.
+ * @param privilege_level
+ *                      The RPL (Requestors Privilege Level) of this gate.
+ */
 extern void idt_setup_interrupt_gate (unsigned int number,
                                       unsigned int selector, 
                                       void *address, 
                                       unsigned int privilege_level);
 
-/* External variables. */
-/* The IDT. */
+/**
+ * @brief The IDT. 
+ */
 extern descriptor_t idt[];
 
 #endif /* !__STORM_IA32_IDT_H__ */
