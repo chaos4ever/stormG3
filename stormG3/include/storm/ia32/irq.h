2dc1f2457cb41722369741ae3836609a3b4c943c
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 20:36:25 +0000

    Added good Doxygen documentation.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@717 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/irq.h
+++ stormG3/include/storm/ia32/irq.h
@@ -1,4 +1,4 @@
-/* $chaos: irq.h,v 1.7 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: irq.h,v 1.8 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -17,47 +17,82 @@
 #include <storm/ia32/process.h>
 #include <storm/ia32/thread.h>
 
-/* Defines. */
-/** @brief The base addresses of 8259-1. */
+/**
+ * @brief               The base addresses of 8259-1. 
+ */
 #define INTERRUPT_CONTROLLER_MASTER     0x20
 
-/** @brief The base address of 8259-2. */
+/**
+ * @brief               The base address of 8259-2. 
+ */
 #define INTERRUPT_CONTROLLER_SLAVE      0xA0
 
-/** @brief The number of IRQ levels. */
+/**
+ * @brief               The number of IRQ levels. 
+ */
 #define IRQ_LEVELS                      16
 
-/* The IRQ:s are mapped to interrupt 0x20 and upward. */
+/**
+ * @brief               The IRQ:s are mapped to interrupt 0x20 and 
+ *                      upward. 
+ */
 #define BASE_IRQ                        0x20
+
+/**
+ * @brief               The IDT entry for IRQ level a.
+ */
 #define IDT_ENTRY(a)                    (BASE_IRQ + a)
 
-/** @brief A structure with information about an IRQ level. */
+/**
+ * @brief               A structure with information about an IRQ level. 
+ */
 typedef struct
 {
-    /** Is this IRQ allocated? */
+    /**
+     * @brief           Is this IRQ allocated? 
+     */
     bool allocated;
     
-    /** A function pointer to the handler. */
+    /**
+     * @brief           A function pointer to the handler. 
+     */
     irq_handler_t *handler;
     
-    /** A short description of the IRQ. */
+    /**
+     * @brief           A short description of the IRQ. 
+     */
     char *description;
 
-    /** The number of currently unhandled interrupts. */
+    /**
+     * @brief           The number of currently unhandled interrupts. 
+     */
     volatile unsigned int interrupts_pending;
   
-    /** The number of times this IRQ has been raised. */
+    /**
+     * @brief           The number of times this IRQ has been raised. 
+     */
     unsigned long long occurred;
 
-    /** Are we currently handling this interrupt? */
+    /**
+     * @brief           Are we currently handling this interrupt? 
+     */
     volatile bool in_handler;
 } irq_t;
 
-/** @brief Initialize interrupt handling, and enable interrupts. */
+/**
+ * @brief               Initialize interrupt handling, and enable
+ *                      interrupts. 
+ */
 extern void irq_init (void);
 
-/* Low-level interrupt handlers. */
+/**
+ * @brief               Low-level IRQ 0 handler. 
+ */
 extern void irq0_handler (void);
+
+/**
+ * @brief               Low-level IRQ 1 handler. 
+ */
 extern void irq1_handler (void);
 extern void irq3_handler (void);
 extern void irq4_handler (void);
@@ -73,7 +108,10 @@ extern void irq13_handler (void);
 extern void irq14_handler (void);
 extern void irq15_handler (void);
 
-/** @brief High-level interrupt handler. */
+/**
+ * @brief               High-level interrupt handler. 
+ * @param irq_number    The number of the IRQ to handle.
+ */
 extern void irq_handler (unsigned int irq_number);
 
 #endif /* !__STORM_IA32_IRQ_H__ */
--- stormG3/include/storm/ia32/memory_physical.h
+++ stormG3/include/storm/ia32/memory_physical.h
@@ -1,12 +1,12 @@
-/* $chaos: memory_physical.h,v 1.9 2002/10/06 09:13:23 per Exp $ */
+/* $chaos: memory_physical.h,v 1.10 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file memory_physical.h
- * @brief Physical memory allocation prototypes.
+ * @file                memory_physical.h
+ * @brief               Physical memory allocation prototypes.
  */
 
 #ifndef __STORM_IA32_MEMORY_PHYSICAL_H__
@@ -15,73 +15,132 @@
 #include <storm/types.h>
 #include <storm/ia32/process.h>
 
-/* Structures. */
-/** @brief A SLAB block is used for keeping a list of all free pages. */
+/** 
+ * @brief               A SLAB block is used for keeping a list of all
+ *                      free pages. 
+ */
 typedef struct 
 {
-    /** Unique cookie, to make sure the data structure is of the right
-       type. */
+    /**
+     * @brief           Unique cookie, to make sure the data structure 
+     *                  is of the right type. */
     uint32_t magic_cookie;
 
-    /** Pointer to next free SLAB. */
+    /**
+     * @brief           Pointer to next free SLAB. 
+     */
     struct memory_physical_slab_t *next;
 } memory_physical_slab_t;
 
-/** @brief A structure used for keeping information about which pages
-    a process has allocated.
-
-    This structure is used for holding information about the pages
-    allocated for a given process. This is very useful for GC:ing the
-    process when it exits. */
+/**
+ * @brief               A structure used for keeping information about
+ *                      which pages a process has allocated.
+ *
+ * This structure is used for holding information about the pages
+ * allocated for a given process. This is very useful for GC:ing the
+ * process when it exits. 
+ */
 typedef struct
 {
-    /** Unique cookie, to make sure the data structure is of the right
-        type. */
+    /**
+     * @brief           Unique cookie, to make sure the data structure is of
+     *                  the right type. 
+     */
     uint32_t magic_cookie;
 
-    /** The process owning this page. */
+    /**
+     * @brief           The process owning this page. 
+     */
     process_id_t process_id;
   
-    /** The physical address of this page. */
+    /**
+     * @brief           The physical address of this page. 
+     */
     void *page_address;
 
-    /** Pointer to the previous page in this list. */
+    /**
+     * @brief           Pointer to the previous page in this list. 
+     */
     struct memory_physical_process_page_t *previous;
 
-    /** Pointer to the next page in this list. */
+    /**
+     * @brief           Pointer to the next page in this list. 
+     */
     struct memory_physical_process_page_t *next;
 } memory_physical_process_page_t;
 
-/* Variables. */
-/** @brief The number of physical pages in the system. */
+/**
+ * @brief               The total number of physical pages in the system. 
+ */
 extern page_number_t physical_pages;
 
-/** @brief The number of free pages. */
+/**
+ * @brief               The number of free pages. 
+ */
 extern unsigned int free_pages;
 
-/* Functions. */
-/** @brief Initialize physical memory allocation. */
-extern void memory_physical_init (void);
+/**
+ * @brief               Initialize physical memory allocation. 
+ */
+void memory_physical_init (void);
 
-/** @brief Allocate a number of pages. */
+/**
+ * @brief               Free pages that was used during startup. 
+ */
+void memory_physical_done (void);
+
+/**
+ * @brief               Allocate a number of pages. 
+ * @param pointer       A double pointer to where the function will put
+ *                      a pointer to the allocated memory.
+ * @param pages         The number of pages to allocate. (1 is O(1),
+ *                      more is much, much slower)
+ * @return              STORM_RETURN_SUCCESS if successful.
+ *
+ * FIXME: Merge this with the next one. The kernel should be treated
+ * as a process with PROCESS_ID_KERNEL. That way, it will be possible
+ * (and very easy) to see exactly how much memory the kernel has
+ * allocated. However... there may be a BIG problem with this; the
+ * information is stored in memory_global_allocate'd memory and that
+ * function gets its memory from memory_physical_allocate, so we would
+ * have a loop... Think about this.
+ */
 return_t memory_physical_allocate (void **pointer, unsigned int pages);
 
-/** @brief Allocate a page for a process. */
+/**
+ * @brief               Allocate a page for a process. 
+ * @param pointer       A double pointer to where the function will put
+ *                      a pointer to the allocated memory.
+ * @param process_id    The process ID to allocate the memory for.
+ * @return              STORM_RETURN_SUCCESS if successful.
+ */
 return_t memory_physical_allocate_for_process (void **pointer, 
                                                process_id_t process_id);
 
-/** @brief Deallocate a page.
-
-    Yes, only one page. If you allocated multiple
-    pages, you need to call this function for each page. I will not
-    keep track of the number of pages you have allocated for you, you
-    will need to do it yourself. */
+/**
+ * @brief               Deallocate a page.
+ * @param pointer       A pointer to the page.
+ * @return              STORM_RETURN_SUCCESS if successful.
+ *
+ * Yes, only one page. If you allocated multiple
+ * pages, you need to call this function for each page. I will not
+ * keep track of the number of pages you have allocated for you, you
+ * will need to do it yourself. 
+ */
 return_t memory_physical_deallocate (void *pointer);
 
-/** @brief Deallocate a page for a process. */
+/**
+ * @brief               Deallocate a page for a process. 
+ * @param pointer       A pointer to the page.
+ * @return              STORM_RETURN_SUCCESS if successful.
+ */
 return_t memory_physical_deallocate_for_process (void *pointer);
 
-/** @brief Free all pages that belongs to the given process. */
+/** 
+ * @brief               Free all pages that belongs to the given process. 
+ * @param process_id    The process ID of the process.
+ * @return              STORM_RETURN_SUCCESS if successful.
+ */
 return_t memory_physical_free_process (process_id_t process_id);
 
 #endif /* !__STORM_IA32_MEMORY_PHYSICAL_H__ */
