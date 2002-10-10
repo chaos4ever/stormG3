95a8aea3efcff12043dd50445e6815d31bbc324b
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 20:37:08 +0000

    Improved the Doxygentation.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@719 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/halt.h
+++ stormG3/include/storm/ia32/halt.h
@@ -1,26 +1,37 @@
-/* $chaos: halt.h,v 1.2 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: halt.h,v 1.3 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file halt.h
- * @brief Functions for halting the machine. 
+ * @file        halt.h
+ * @brief       Functions for halting the machine. 
  */
 
 #ifndef __STORM_IA32_HALT_H__
 #define __STORM_IA32_HALT_H__
 
+/**
+ * @brief       Different ways of halting.
+ */
 enum halt_t {
-    /* Reboot the machine. */
+    /**
+     * @brief   Reboot the machine. 
+     */
     HALT_REBOOT,
 
-    /* Shut it down (with possible power-down as well). */
+    /**
+     * @brief   Shut it down (with possible power-down as well). 
+     */
     HALT_SHUTDOWN
 };
 
-/* Halt the machine. */
+/**
+ * @brief               Halt the machine. 
+ * @param type          The halt type we want.
+ * @return              It won't return. :-)
+ */
 extern return_t halt (enum halt_t type);
 
 #endif /* !__STORM_IA32_HALT_H__ */
