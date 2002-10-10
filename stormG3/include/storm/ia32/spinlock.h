44d679f3835d2f6507f2e3f124812a9e587bd97a
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 20:35:34 +0000

    Merged with lock.h
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@715 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/spinlock.h
+++ stormG3/include/storm/ia32/spinlock.h
@@ -1,49 +1,89 @@
-/* $chaos: spinlock.h,v 1.2 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: spinlock.h,v 1.3 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file spinlock.h
- * @brief Kernel locking. 
+ * @file                spinlock.h
+ * @brief               Kernel locking. 
  */
 
 #ifndef __STORM_IA32_SPINLOCK_H__
 #define __STORM_IA32_SPINLOCK_H__
 
-/* Type definitions. */
+/**
+ * @brief               A spin lock.
+ */
 typedef unsigned int spinlock_t;
 
-/* Enumerations. */
-enum
+/**
+ * @brief               The setting of a lock.
+ */
+enum spinlock_t
 {
+    /**
+     * @brief           The lock is locked.
+     */
     SPIN_LOCKED,
+
+    /**
+     * @brief           The lock is unlocked.
+     */
     SPIN_UNLOCKED
 };
 
-/* Lock the given spinlock. This function loops until it may access
-   the semaphores, and then locks them. */
-static inline void spin_lock (spinlock_type spinlock)
+/** 
+ * @brief               Get the lock. 
+ * @param lock          The lock to get.
+ */
+static inline void spin_lock (spinlock_t *lock)
 {
     unsigned int eax;
     
     asm volatile ("movl $0, %%eax\n"
                   "0:  xchgl %%eax, %0\n"
                   "cmpl $0, %%eax\n"
-                  "je 0b"
-                  :
-                  "=g" (spinlock),
+                  "jne 1f\n"
+                  "hlt\n"               /* This one is the key. */
+                  "jmp 0b\n"
+                  "1:"
+                  : 
+                  "=g" (*lock),
                   "=a" (eax)
                   :
-                  "bcdDS" (spinlock));
+                  "bcdDS" (*lock));
 }
 
-/* Unlock a mutex which has previously been locked with
-   mutex_lock. */
-static inline void spin_unlock (spinlock_type spinlock)
+/**
+ * @brief               Get the look, from code that MAY BE run from
+ *                      within an interrupt handler. 
+ * @param lock          The lock to get.
+ *
+ */
+static inline void spin_lock_interrupt (spinlock_t *lock)
 {
-    spinlock = SPIN_UNLOCKED;
+    unsigned int eax;
+    
+    asm volatile ("movl $0, %%eax\n"
+                  "0:  xchgl %%eax, %0\n"
+                  "cmpl $0, %%eax\n"
+                  "je 0b\n"
+                  : 
+                  "=g" (*lock),
+                  "=a" (eax)
+                  :
+                  "bcdDS" (*lock));
+}
+
+/**
+ * @brief               Unlock a spinlock which has previously been 
+ *                      locked with spin_lock. 
+ * @param lock          The lock to unlock.
+ */
+static inline void spin_unlock (spinlock_t *lock)
+{
+    *lock = SPIN_UNLOCKED;
 }
 
 #endif /* !__STORM_IA32_SPINLOCK_H__ */
