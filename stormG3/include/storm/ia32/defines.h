4013e262cf24d90e06b3cf946a13e558c73a97d7
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 21:43:25 +0000

    Properly indented doxygentation.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@729 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/defines.h
+++ stormG3/include/storm/ia32/defines.h
@@ -1,4 +1,4 @@
-/* $chaos: defines.h,v 1.17 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: defines.h,v 1.18 2002/10/10 13:03:03 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
 
@@ -17,27 +17,28 @@
 #include <storm/types.h>
 
 /**
- * @brief       The size of a physical page. 
+ * @brief               The size of a physical page. 
  */
 #define PAGE_SIZE                       0x1000
 
 /**
- * @brief       The page number of an address, shifted 10 bits to the left.
-*/
+ * @brief               The page number of an address, shifted 10 bits
+ *                      to the left.
+ */
 #define PAGE_ADDRESS(x)                 ((void *) (((uint32_t) x) & 0xFFFFF000))
 
 /**
- * @brief       Size of some regions. 
+ * @brief               Size of some regions. 
  */
 #define KERNEL_STACK_SIZE               (4 * KIB)
 
 /**
- * @brief       The kernel starts at 1 MiB. 
+ * @brief               The kernel starts at 1 MiB. 
  */
 #define KERNEL_BASE                     (1 * MIB)
 
 /**
- * @brief       The maximum memory amount that we support. 
+ * @brief               The maximum memory amount that we support. 
  */
 #define MAX_MEMORY                      ((uint32_t) 2 * GIB)
 
