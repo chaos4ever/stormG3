de990cac3b86e1d52920b6b7981fb8417040f9b0
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-10 21:39:38 +0000

    New structure: thread_t
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@726 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/thread.h
+++ stormG3/include/storm/ia32/thread.h
@@ -1,21 +1,60 @@
-/* $chaos: thread.h,v 1.2 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: thread.h,v 1.3 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file thread.h
- * @brief Thread support. 
+ * @file                thread.h
+ * @brief               Thread support. 
  */
 
 #ifndef __STORM_IA32_THREAD_H__
 #define __STORM_IA32_THREAD_H__
 
-/* The thread ID of the kernel thread. (idle thread) */
+#include <storm/ia32/tss.h>
+
+/**
+ * @brief               The thread ID of the kernel thread. (idle 
+ *                      thread) 
+ */
 #define THREAD_ID_KERNEL                0
 
-/* Type definitions. */
+/**
+ * @brief               Type definitions. 
+ */
 typedef unsigned int thread_id_t;
+
+/**
+ * @brief               A thread.
+ */
+typedef struct
+{
+    /**
+     * @brief           The thread ID.
+     */
+    thread_id_t thread_id;
+
+    /**
+     * @brief           A lock, to make sure the integrity of the data is
+     *                  maintained.
+     */
+    spinlock_t lock;
+
+    /**
+     * @brief           The TSS for this thread.
+     */
+    tss_t *tss;
+
+    /**
+     * @brief           The previous thread for this process.
+     */
+    struct thread_t *previous;
+
+    /**
+     * @brief           The next thread for this process.
+     */
+    struct thread_t *next;
+} thread_t;
 
 #endif /* !__STORM_IA32_THREAD_H__ */
