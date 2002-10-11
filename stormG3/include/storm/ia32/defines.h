ddb233f3b5aa76151e121a2a9c9e45e090c9e084
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-11 07:44:26 +0000

    New macro: PAGE_NUMBER (for getting the page number of an address).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@740 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/defines.h
+++ stormG3/include/storm/ia32/defines.h
@@ -1,4 +1,4 @@
-/* $chaos: defines.h,v 1.18 2002/10/10 13:03:03 per Exp $ */
+/* $chaos: defines.h,v 1.19 2002/10/10 21:43:25 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
 
@@ -6,8 +6,8 @@
 /* Use freely under the terms listed in the file LICENSE. */
 
 /** 
- * @file        ia32/defines.h
- * @brief       IA32-specific storm defines. 
+ * @file                ia32/defines.h
+ * @brief               IA32-specific storm defines. 
  */
 
 #ifndef __STORM_IA32_DEFINES_H__
@@ -19,27 +19,38 @@
 /**
  * @brief               The size of a physical page. 
  */
-#define PAGE_SIZE                       0x1000
+#define PAGE_SIZE       0x1000
+
+/**
+ * @brief               The number of bits of a page.
+ */
+#define PAGE_BITS       10
 
 /**
  * @brief               The page number of an address, shifted 10 bits
  *                      to the left.
  */
-#define PAGE_ADDRESS(x)                 ((void *) (((uint32_t) x) & 0xFFFFF000))
+#define PAGE_ADDRESS(x) ((void *) (((uint32_t) x) & 0xFFFFF000))
+
+/**
+ * @brief               The page number of an address.
+ */
+#define PAGE_NUMBER(x)  (((address_t) x) >> PAGE_BITS)
 
 /**
  * @brief               Size of some regions. 
  */
-#define KERNEL_STACK_SIZE               (4 * KIB)
+#define KERNEL_STACK_SIZE \
+                        (4 * KIB)
 
 /**
  * @brief               The kernel starts at 1 MiB. 
  */
-#define KERNEL_BASE                     (1 * MIB)
+#define KERNEL_BASE     (1 * MIB)
 
 /**
  * @brief               The maximum memory amount that we support. 
  */
-#define MAX_MEMORY                      ((uint32_t) 2 * GIB)
+#define MAX_MEMORY      ((uint32_t) 2 * GIB)
 
 #endif /* !__STORM_IA32_DEFINES_H__ */
