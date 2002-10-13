2353501be194c1aa440821ba28e5c34b6ed32adb
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-13 13:55:12 +0000

    Added Doxygen documentation. New prototype: dispatch_init
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@745 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/dispatch.h
+++ stormG3/include/storm/ia32/dispatch.h
@@ -1,21 +1,30 @@
-/* $chaos: dispatch.h,v 1.3 2002/10/04 19:01:20 per Exp $ */
+/* $chaos: dispatch.h,v 1.4 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file dispatch.h
- * @brief Dispatcher prototypes.
+ * @file                dispatch.h
+ * @brief               Dispatcher prototypes.
  */
 
 #ifndef __STORM_IA32_DISPATCH_H__
 #define __STORM_IA32_DISPATCH_H__
 
-/* The task switcher -- IRQ0 handler. */
-extern void dispatch_task_switcher (void);
+/**
+ * @brief               Initialize the dispatcher. 
+ */
+void dispatch_init (void);
 
-/* The number of ticks. */
+/**
+ * @brief               The task switcher -- IRQ0 handler. 
+ */
+void dispatch_task_switcher (void);
+
+/**
+ * @brief               The number of ticks. 
+ */
 extern volatile unsigned int ticks;
 
 #endif /* !__STORM_IA32_DISPATCH_H__ */
