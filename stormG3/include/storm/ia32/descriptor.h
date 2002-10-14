ed2bde85eabd5ebd8e454242cdbdf8f8a7abce30
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-14 21:29:50 +0000

    Doxymentation.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@751 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/descriptor.h
+++ stormG3/include/storm/ia32/descriptor.h
@@ -1,4 +1,4 @@
-/* $chaos: descriptor.h,v 1.4 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: descriptor.h,v 1.5 2002/10/10 21:42:55 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -11,8 +11,11 @@
 #define __STORM_IA32_DESCRIPTOR_H__
 
 #include <storm/defines.h>
-#include <storm/types.h>
+#include <storm/ia32/types.h>
 
+/**
+ * @brief               Descriptor types.
+ */
 enum descriptor_type
 {
     /**
@@ -36,7 +39,8 @@ enum descriptor_type
     DESCRIPTOR_TYPE_INTERRUPT_GATE_32 = 14,
 
     /**
-     * @brief           A 32-bit trap gate. */
+     * @brief           A 32-bit trap gate. 
+     */
     DESCRIPTOR_TYPE_TRAP_GATE_32 = 15,
 };
 
@@ -45,20 +49,68 @@ enum descriptor_type
  */
 typedef struct
 {
-    uint16_t limit_lo;                  /* Low 16 bits of segment limit. */
-    uint16_t base_lo;                   /* Low 16 bits of base address. */
-    uint8_t base_hi;                    /* Bits 16-23 of base address. */
-    uint8_t type : 4;                   /* Segment type. */
-    uint8_t descriptor_type : 1;        /* 0 = system, 1 = code or data. */
-    uint8_t dpl : 2;                    /* Descriptor privilege level. */
-    uint8_t segment_present : 1;        /* Zero if segment isn't used. */
-    uint8_t limit_hi : 4;               /* High four bits of segment limit. */
-    uint8_t : 1;                        /* Unused. */
-    uint8_t zero : 1;                   /* Always zero! */
-    uint8_t operation_size : 1;         /* 0 = 16-bit, 1 = 32-bit. */
-    uint8_t granularity : 1;            /* If 1, segment limit is multiplied
-                                          by 4096. */
-    uint8_t base_hi2;                   /* Bits 24-31 of base address. */
+    /**
+     * @brief           Low 16 bits of segment limit. 
+     */
+    uint16_t limit_lo;
+
+    /**
+     * @brief           Low 16 bits of base address. 
+     */
+    uint16_t base_lo;
+
+    /**
+     * @brief           Bits 16-23 of base address. 
+     */
+    uint8_t base_hi;
+
+    /**
+     * @brief           Segment type. 
+     */
+    uint8_t type : 4;
+
+    /**
+     * @brief           0 = system, 1 = code or data. 
+     */
+    uint8_t descriptor_type : 1;
+
+    /**
+     * @brief           Descriptor privilege level. 
+     */
+    uint8_t dpl : 2;
+
+    /**
+     * @brief           Zero if segment isn't used. 
+     */
+    uint8_t segment_present : 1;
+
+    /**
+     * @brief           High four bits of segment limit. 
+     */
+    uint8_t limit_hi : 4;
+
+    /* Unused. */
+    uint8_t : 1;
+
+    /**
+     * @brief           Always zero! 
+     */
+    uint8_t zero : 1;
+
+    /**
+     * @brief           0 = 16-bit, 1 = 32-bit. 
+     */
+    uint8_t operation_size : 1;
+
+    /**
+     * @brief           If 1, segment limit is multiplied by 4096. 
+     */
+    uint8_t granularity : 1;
+
+    /**
+     * @brief           Bits 24-31 of base address. 
+     */
+    uint8_t base_hi2;
 } descriptor_t PACKED;
 
 /**
@@ -66,15 +118,37 @@ typedef struct
  */
 typedef struct
 {
-    uint16_t offset_lo;                 /* Low 16 bits of offset. */
+    /**
+     * @brief           Low 16 bits of offset. 
+     */
+    uint16_t offset_lo;
     uint16_t segment_selector;
-    uint8_t params : 5;                 /* Zero if not type == 12. */
-    uint8_t zero : 3;                   /* Zero */
+
+    /**
+     * @brief           Zero if not type == 12. 
+     */
+    uint8_t params : 5;
+
+    /*
+     * @brief           Zero 
+     */
+    uint8_t zero : 3;
     uint8_t type : 5;                 
-    uint8_t dpl : 2;                    /* Descriptor privilege level. */
-    uint8_t present : 1;                /* If zero, this descriptor isn't
-                                             used. */
-    uint16_t offset_hi;                 /* High 16 bits of offset. */
+
+    /**
+     * @brief           Descriptor privilege level. 
+     */
+    uint8_t dpl : 2;
+
+    /**
+     * @brief           If zero, this descriptor isn't used. 
+     */
+    uint8_t present : 1;
+
+    /**
+     * @brief           High 16 bits of offset. 
+     */
+    uint16_t offset_hi;
 } gate_descriptor_t PACKED;
 
 #endif /* !__STORM_IA32_DESCRIPTOR_H__ */
