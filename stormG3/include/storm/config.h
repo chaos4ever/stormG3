34e81d8a823738cf9ed0be09855560433d594bc8
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-17 19:24:59 +0000

    Bumped the version number to 0.0.3 since we have process support now (not fully working yet though).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@798 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/config.h
+++ stormG3/include/storm/config.h
@@ -1,20 +1,20 @@
-/* $chaos: config.h,v 1.6 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: config.h,v 1.7 2002/10/09 18:52:27 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file config.h 
- * @brief Kernel configuration. 
+ * @file                config.h
+ * @brief               Kernel configuration. 
  */
 
 #ifndef __STORM_CONFIG_H__
 #define __STORM_CONFIG_H__
 
 /**
- * @brief The version of storm. 
+ * @brief               The version of storm. 
  */
-#define STORM_VERSION   "G3 0.0.2"
+#define STORM_VERSION   "G3 0.0.3"
 
 #endif /* !__STORM_CONFIG_H__ */
