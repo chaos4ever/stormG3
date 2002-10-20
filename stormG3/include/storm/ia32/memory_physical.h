7589f5ad23bb2f86d31f226c8f880be45eaeefd1
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-20 19:32:43 +0000

    New field: page_process_t::pages Got rid of _for_process functions; merged them with the main functions instead.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@818 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/memory_physical.h
+++ stormG3/include/storm/ia32/memory_physical.h
@@ -1,4 +1,4 @@
-/* $chaos: memory_physical.h,v 1.10 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: memory_physical.h,v 1.11 2002/10/10 20:36:08 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -46,27 +46,34 @@ typedef struct
      * @brief           Unique cookie, to make sure the data structure is of
      *                  the right type. 
      */
-    uint32_t magic_cookie;
+    uint32_t            magic_cookie;
 
     /**
-     * @brief           The process owning this page. 
+     * @brief           The process owning this page/these pages.
      */
-    process_id_t process_id;
+    process_id_t        process_id;
   
     /**
-     * @brief           The physical address of this page. 
+     * @brief           The physical address of this page/these pages. 
      */
-    void *page_address;
+    void                *page_address;
+
+    /**
+     * @brief           The number of pages this node contains.
+     */
+    size_t              pages;
 
     /**
      * @brief           Pointer to the previous page in this list. 
      */
-    struct memory_physical_process_page_t *previous;
+    struct memory_physical_process_page_t 
+                        *previous;
 
     /**
      * @brief           Pointer to the next page in this list. 
      */
-    struct memory_physical_process_page_t *next;
+    struct memory_physical_process_page_t
+                        *next;
 } memory_physical_process_page_t;
 
 /**
@@ -95,17 +102,14 @@ void memory_physical_done (void);
  *                      a pointer to the allocated memory.
  * @param pages         The number of pages to allocate. (1 is O(1),
  *                      more is much, much slower)
+ * @param process_id    The process ID for which to allocate the memory. Specify
+ *                      PROCESS_ID_NONE if you do not wish this to be stored in
+ *                      the list (it will leak the page if the process get
+ *                      killed in a weird way).
  * @return              STORM_RETURN_SUCCESS if successful.
- *
- * FIXME: Merge this with the next one. The kernel should be treated
- * as a process with PROCESS_ID_KERNEL. That way, it will be possible
- * (and very easy) to see exactly how much memory the kernel has
- * allocated. However... there may be a BIG problem with this; the
- * information is stored in memory_global_allocate'd memory and that
- * function gets its memory from memory_physical_allocate, so we would
- * have a loop... Think about this.
  */
-return_t memory_physical_allocate (void **pointer, unsigned int pages);
+return_t memory_physical_allocate (void **pointer, unsigned int pages,
+                                   process_id_t process_id);
 
 /**
  * @brief               Allocate a page for a process. 
@@ -114,8 +118,7 @@ return_t memory_physical_allocate (void **pointer, unsigned int pages);
  * @param process_id    The process ID to allocate the memory for.
  * @return              STORM_RETURN_SUCCESS if successful.
  */
-return_t memory_physical_allocate_for_process (void **pointer, 
-                                               process_id_t process_id);
+return_t memory_physical_allocate_for_process (void **pointer) DEPRECATED;
 
 /**
  * @brief               Deallocate a page.
@@ -128,13 +131,6 @@ return_t memory_physical_allocate_for_process (void **pointer,
  * will need to do it yourself. 
  */
 return_t memory_physical_deallocate (void *pointer);
-
-/**
- * @brief               Deallocate a page for a process. 
- * @param pointer       A pointer to the page.
- * @return              STORM_RETURN_SUCCESS if successful.
- */
-return_t memory_physical_deallocate_for_process (void *pointer);
 
 /** 
  * @brief               Free all pages that belongs to the given process. 
