fdcaf2e145c05b512e5bf1c73a01326e66bf78ec
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 21:05:57 +0000

    Set up current-arch symlinks.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@860 1f819a86-672b-0410-b5ed-9de735de380c

--- compile.sh
+++ compile.sh
@@ -1,9 +1,22 @@
 # $Id$
 # Abstract: Quick and dirty hack to compile everything.
 
+# The source root
 ROOT=$(pwd)
+INSTALL_PREFIX=/tftpboot
+
+# Set up the link so we will know which architecture is the
+# current. FIXME: Add an architecture check perhaps?? :-)
+rm -f $ROOT/stormG3/include/storm/current-arch
+ln -sf $ROOT/stormG3/include/storm/ia32 $ROOT/stormG3/include/storm/current-arch
+
 cd $ROOT/stormG3 || exit
 cons $STORM_OPTIONS || exit
+
+# Set up the link in the installed root as well.
+rm -f $INSTALL_PREFIX/data/programming/c/headers/storm/current-arch
+ln -sf $INSTALL_PREFIX/data/programming/c/headers/storm/ia32 $INSTALL_PREFIX/data/programming/c/headers/storm/current-arch
+
 cd $ROOT/libraries
 cons || exit
 cd $ROOT/programs
