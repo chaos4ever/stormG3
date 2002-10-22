dd57ce71684b8b97235bc1ef71775d9bbf037cec
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 20:58:07 +0000

    New type: irq_handler_t New type: process_id_t New type: service_info_t New type: service_t
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@853 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/types.h
+++ stormG3/include/storm/types.h
@@ -1,4 +1,4 @@
-/* $chaos: types.h,v 1.10 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: types.h,v 1.11 2002/10/22 19:42:45 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org>
            Henrik Hallin <hal@chaosdev.org> */
 
@@ -12,6 +12,7 @@
 #define __STORM_TYPES_H__
 
 #include <stdint.h>
+#include <storm/defines.h>
 
 /**
  * @brief               A size. 
@@ -28,11 +29,69 @@ typedef int return_t;
  */
 typedef return_t (*function_t)(void);   /* Can be used even if the
                                            function uses arguments. */
+
+/**
+ * @brief               An IRQ handler.
+ */
+typedef void (irq_handler_t)(unsigned int irq_level);
+
+/**
+ * @brief               A unique process ID.
+ */
+typedef unsigned int    process_id_t;
+
 /**
  * @brief               A boolean value.
  */
 typedef int bool;                       /* no _t, since this should
                                            really have been a built-in
                                            type in C. */
+
+/**
+ * @brief                               A function that provides info about
+ *                                      this service provider. The data
+ *                                      returned is protocol specific.
+ */
+typedef return_t (*service_info_t)(void *);
+
+/**
+ * @brief               A structure used for returning information to the
+ *                      caller of service_resolve()
+ */
+typedef struct
+{
+    /**
+     * @brief           The service name. 
+     */
+    char                name[SERVICE_NAME_LENGTH];
+
+    /**
+     * @brief           The vendor name. 
+     */
+    char                vendor[SERVICE_VENDOR_LENGTH];
+
+    /**
+     * @brief           The model name. 
+     */
+    char                model[SERVICE_MODEL_LENGTH];
+
+    /**
+     * @brief           Unique ID for this device. (MAC address, serial
+     *                  number, etc). Used to distinguish between 
+     *                  different devices with the same model. 
+     */
+    char                id[SERVICE_ID_LENGTH];
+
+    /**
+     * @brief           The version of the service provided. 
+     */
+    unsigned int        version;
+
+    /**
+     * @brief           A function that the caller use to get information
+     *                  about this service provider. 
+     */
+    service_info_t      service_info;
+} service_t;
 
 #endif /* !__STORM_TYPES_H__ */
