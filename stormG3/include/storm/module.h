876278be57b09a08005ce76d978fd4b95bafeec3
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 21:02:52 +0000

    Moved module_start() prototype to interface.h where it belongs.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@858 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/module.h
+++ stormG3/include/storm/module.h
@@ -1,12 +1,12 @@
-/* $chaos: module.h,v 1.4 2002/10/04 19:01:19 per Exp $ */
+/* $chaos: module.h,v 1.5 2002/10/08 20:14:56 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file storm/module.h
- * @brief Functions and variables defined by a module. 
+ * @file                storm/module.h
+ * @brief               Functions and variables defined by a module. 
  */
 
 #ifndef __STORM_MODULE_H__
@@ -19,30 +19,19 @@
 #define MODULE_VERSION_LENGTH           16
 
 /** 
- * @brief A module information structure. 
+ * @brief               A module information structure. 
  */
 typedef struct
 {
     /** 
-     * @brief The module name.
+     * @brief           The module name.
      */
-    char name[MODULE_NAME_LENGTH];
+    char                name[MODULE_NAME_LENGTH];
     
     /**
-     * @brief The module version. 
+     * @brief           The module version. 
      */
-    char version[MODULE_VERSION_LENGTH];
+    char                version[MODULE_VERSION_LENGTH];
 } module_info_t;
-
-#ifndef STORM /* Only when included by a module. */
-
-/** 
- * @brief The module entry point. 
- */
-extern return_t module_start (void);
-//extern return_t module_info (module_info_t *module_info);
-//extern char module_name[];
-
-#endif /* !STORM */
 
 #endif /* !__STORM_MODULE_H__ */
