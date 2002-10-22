5c692834346d3c7dfe988fc08e1d7b1bea14fd68
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 19:42:45 +0000

    Improved the spacing a bit.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@841 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/types.h
+++ stormG3/include/storm/types.h
@@ -1,28 +1,36 @@
-/* $chaos: types.h,v 1.9 2002/10/04 19:01:19 per Exp $ */
+/* $chaos: types.h,v 1.10 2002/10/08 20:16:14 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org>
            Henrik Hallin <hal@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
 /* Use freely under the terms listed in the file LICENSE. */
 
-/** @file storm/types.h
-    @brief storm types. */
+/** @file               storm/types.h
+    @brief              Architecture-independent storm types. */
 
 #ifndef __STORM_TYPES_H__
 #define __STORM_TYPES_H__
 
 #include <stdint.h>
 
-/* Type definitions. */
-/** @brief A size. */
+/**
+ * @brief               A size. 
+ */
 typedef int size_t;
 
-/** @brief A return value (STORM_RETURN_*) */
+/**
+ * @brief               A return value (STORM_RETURN_*) 
+ */
 typedef int return_t;
 
-/** @brief A function pointer. */
+/**
+ * @brief               A function pointer. 
+ */
 typedef return_t (*function_t)(void);   /* Can be used even if the
                                            function uses arguments. */
+/**
+ * @brief               A boolean value.
+ */
 typedef int bool;                       /* no _t, since this should
                                            really have been a built-in
                                            type in C. */
