8e25153dbc520946b14667f3c202164359b3a8d9
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 20:57:27 +0000

    New file; used for getting all the architecture-specific includes (since they might differ between architectures).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@852 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ stormG3/include/storm/ia32/storm.h
@@ -0,0 +1,18 @@
+/* $chaos: dotfile.emacs,v 1.37 2002/10/14 16:03:33 per Exp $ */
+/* Abstract: IA32-specific includes. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file LICENSE. */
+
+/**
+ * @file             ia32/storm.h
+ * @brief            IA32-specific include files are included through this file.
+ */
+
+#ifndef __STORM_IA32_STORM_H__
+#define __STORM_IA32_STORM_H__
+
+#include <storm/ia32/types.h>
+
+#endif /* !__STORM_IA32_STORM_H__ */
