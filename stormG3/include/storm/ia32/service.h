c20f4c93efa78f80e5c495620ae30d30c9778a35
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-22 20:56:26 +0000

    Moved lots of the code to storm/interface.h so that it can be properly exported in the kernel ABI.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@849 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/service.h
+++ stormG3/include/storm/ia32/service.h
@@ -1,4 +1,4 @@
-/* $chaos: service.h,v 1.9 2002/10/09 13:07:24 per Exp $ */
+/* $chaos: service.h,v 1.10 2002/10/09 19:03:25 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -12,33 +12,8 @@
 #ifndef __STORM_IA32_SERVICE_H__
 #define __STORM_IA32_SERVICE_H__
 
-#include <storm/ia32/types.h>
+#include <storm/storm.h>
 
-/**
- * @brief The length of a service name.
- */
-#define SERVICE_NAME_LENGTH             32
-
-/**
- * @brief The length of a vendor name.
- */
-#define SERVICE_VENDOR_LENGTH           32
-
-/**
- * @brief The length of a model name.
- */
-#define SERVICE_MODEL_LENGTH            32
-
-/**
- * @brief The length of a unique service ID.
- */
-#define SERVICE_ID_LENGTH               16
-
-/**
- * @brief A function that provides info about this service
- * provider. The data returned is protocol specific.
- */
-typedef return_t (*service_info_t)(void *);
 
 /**
  * @brief Information about a service provider. 
@@ -83,63 +58,5 @@ typedef struct
      */
     struct service_data_t *next;
 } service_data_t;
-
-/**
- * @brief A structure used for returning information to the caller. 
- */
-typedef struct
-{
-    /**
-     * @brief The service name. 
-     */
-    char name[SERVICE_NAME_LENGTH];
-
-    /**
-     * @brief The vendor name. 
-     */
-    char vendor[SERVICE_VENDOR_LENGTH];
-
-    /**
-     * @brief The model name. 
-     */
-    char model[SERVICE_MODEL_LENGTH];
-
-    /**
-     * @brief unique ID for this device. (MAC address, serial number,
-     * etc) Used to distinguish between different devices with the
-     * same model. 
-     */
-    char id[SERVICE_ID_LENGTH];
-
-    /**
-     * @brief The version of the service provided. 
-     */
-    unsigned int version;
-
-    /**
-     * @brief A function that the caller use to get information about this
-     * service provider. 
-     */
-    service_info_t service_info;
-} service_t;
-
-/**
- * @brief Register a service provider. 
- */
-extern return_t service_register (char *name, char *vendor, char *model, 
-                                  char *id, unsigned int version,
-                                  service_info_t service_info);
-
-/**
- * @brief Unregister a service provider. 
- */
-extern return_t service_unregister (char *service, function_t handler);
-
-/** 
- * @brief Lookup a service. 
- */
-extern return_t service_lookup (char *name, char *vendor, char *model,
-                                char *id, unsigned int version, 
-                                size_t *services, service_t **service);
 
 #endif /* !__STORM_IA32_SERVICE_H__ */
