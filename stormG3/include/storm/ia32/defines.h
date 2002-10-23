cc9c80adf7b36bd83aef15b1cf5ba9d727102dcf
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-23 07:26:06 +0000

    Added page directory/table flags (PAGE_*)
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@875 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/defines.h
+++ stormG3/include/storm/ia32/defines.h
@@ -1,4 +1,4 @@
-/* $chaos: defines.h,v 1.22 2002/10/15 21:56:57 per Exp $ */
+/* $chaos: defines.h,v 1.23 2002/10/22 20:56:52 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> 
            Henrik Hallin <hal@chaosdev.org> */
 
@@ -55,6 +55,53 @@
  * @brief               The maximum memory amount that we support. 
  */
 #define MAX_MEMORY      ((uint32_t) 2 * GIB)
+
+/* Page directory/table flags. */
+/**
+ * @brief               Pages owned by the kernel.
+ *
+ * Supervisor-only, read-only (since we don't set WP bit, it isn't
+ * really read-only for the kernel..), and global. 
+ */
+#define PAGE_KERNEL     PAGE_GLOBAL
+
+/**
+ * @brief               Pages owned by user processes.
+ */
+#define PAGE_USER       (PAGE_NON_PRIVILEGED | PAGE_WRITABLE | \
+                         PAGE_WRITE_THROUGH)
+
+/**
+ * @brief               Is the page writable?
+ */
+#define PAGE_WRITABLE   BIT_VALUE (0)
+
+/**
+ * @brief               Can the page be accessed from nonprivileged (ring 3)
+ *                      code?
+ */
+#define PAGE_NON_PRIVILEGED \
+                        BIT_VALUE (1)
+
+/**
+ * @brief               Is write-through cache enabled for the page?
+ */
+#define PAGE_WRITE_THROUGH \
+                        BIT_VALUE (2)
+
+/**
+ * @brief               Is caching completely disabled for this page?
+ *
+ * This flag is neccessary to use when mapping I/O devices.
+ */
+#define PAGE_CACHE_DISABLE \
+                        BIT_VALUE (3)
+
+/**
+ * @brief               Is this a global page, shared between all page
+ *                      directories?
+ */
+#define PAGE_GLOBAL     BIT_VALUE (4)
 
 #endif /* (defined __STORM_KERNEL__) || (defined __STORM_KERNEL_MODULE__) */
 
