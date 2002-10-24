a80a43748c2dbb73d5566e4ba861fc48d8702c5b
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-24 21:37:22 +0000

    Removed much code that's now in storm/ia32/defines.h or storm/ia32/types.h
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@915 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/capability.h
+++ stormG3/include/storm/ia32/capability.h
@@ -1,4 +1,4 @@
-/* $chaos: capability.h,v 1.1 2002/10/17 21:28:37 per Exp $ */
+/* $chaos: capability.h,v 1.2 2002/10/23 21:05:07 per Exp $ */
 /* Abstract: Capabilities support. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -14,18 +14,6 @@
 #define __STORM_IA32_CAPABILITY_H__
 
 /**
- * @brief               The length of a capability ID.
- */
-#define CAPABILITY_CLASS_LENGTH \
-                        (16)
-
-/**
- * @brief               The length of a capability name.
- */
-#define CAPABILITY_NAME_LENGTH \
-                        (32)
-
-/**
  * @brief               The name of the kernel capability class.
  */
 #define CAPABILITY_CLASS_KERNEL \
@@ -36,36 +24,5 @@
  */
 #define CAPABILITY_SUPER_USER \
                         "super_user"
-
-/**
- * @brief               A capability.
- *
- * We could eventually move this to a more advanced data structure
- * where the capability_list is rather a list of the capability
- * classes, and the capability classes hold a list of all the
- * capabilities in this class.
- */
-typedef struct
-{
-    /**
-     * @brief           The capability ID.
-     */
-    char                class[CAPABILITY_CLASS_LENGTH];
-
-    /**
-     * @brief           The capability name (must be unique).
-     */
-    char                name[CAPABILITY_CLASS_LENGTH];
-
-    /**
-     * @brief           Pointer to the previous capability.
-     */
-    struct capability_t *previous;
-
-    /**
-     * @brief           Pointer to the next capability.
-     */
-    struct capability_t *next;
-} capability_t;
 
 #endif /* !__STORM_IA32_CAPABILITY_H__ */
