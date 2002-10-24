b50200c8564d830335c8c77226fce6959cd0b48a
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-24 21:37:41 +0000

    Reformatted the file.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@916 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/idt.h
+++ stormG3/include/storm/ia32/idt.h
@@ -1,4 +1,4 @@
-/* $chaos: idt.h,v 1.5 2002/10/08 20:16:14 per Exp $ */
+/* $chaos: idt.h,v 1.6 2002/10/10 20:36:39 per Exp $ */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
 /* Copyright 2002 chaos development. */
@@ -17,12 +17,12 @@
 /**
  * @brief               The number of entries in the IDT. 
  */
-#define IDT_ENTRIES                     (256)
+#define IDT_ENTRIES     (256)
 
 /**
  * @brief               The size of the IDT.
  */
-#define IDT_SIZE                        (IDT_ENTRIES * sizeof (descriptor_t))
+#define IDT_SIZE        (IDT_ENTRIES * sizeof (descriptor_t))
 
 /**
  * @brief               Create a trap gate in the IDT. 
@@ -38,7 +38,7 @@ extern void idt_setup_trap_gate (unsigned int number,
                                  unsigned int privilege_level);
 
 /**
- * @brief Create an interrupt gate in the IDT. 
+ * @brief               Create an interrupt gate in the IDT. 
  * @param number        The number of the IDT entry.
  * @param selector      The selector to point at.
  * @param address       The address of the function to call as the
@@ -52,8 +52,8 @@ extern void idt_setup_interrupt_gate (unsigned int number,
                                       unsigned int privilege_level);
 
 /**
- * @brief The IDT. 
+ * @brief               The IDT. 
  */
-extern descriptor_t idt[];
+extern descriptor_t     idt[];
 
 #endif /* !__STORM_IA32_IDT_H__ */
