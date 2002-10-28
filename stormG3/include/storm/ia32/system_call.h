47f1d52df6d30915fc037a0fc8cafe7b8ded74ec
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-28 22:08:16 +0000

    system_call returns return_t New inline function: system_call_service_lookup()
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@952 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/include/storm/ia32/system_call.h
+++ stormG3/include/storm/ia32/system_call.h
@@ -1,4 +1,4 @@
-/* $chaos: system_call.h,v 1.1 2002/10/24 21:31:29 per Exp $ */
+/* $chaos: system_call.h,v 1.2 2002/10/24 22:13:08 per Exp $ */
 /* Abstract: System call prototypes etc. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -34,9 +34,42 @@ extern void system_call_lowlevel (void);
 
 /**
  * @brief               The high-level system call handler.
+ * @param stack         The callers stack (with the parameters to the
+ *                      system call).
+ * @return              The return value of the system call.
  */
-extern void system_call (uint32_t *stack);
+extern return_t system_call (uint32_t *stack);
 
 #endif /* __STORM_KERNEL__ */
+
+#if (! defined __STORM_KERNEL__ ) && (! defined __STORM_KERNEL_MODULE__)
+
+/* Lookup a service by name, vendor, model and ID. */
+static inline unsigned int system_call_service_lookup (service_lookup_t *service_lookup, size_t *services, service_t **out_service)
+{
+    unsigned int return_value;
+
+    asm volatile ("pushl        %1\n"   /* out_service */
+                  "pushl        %2\n"   /* services */
+                  "pushl        %3\n"   /* service_lookup */
+
+                  "pushl        %4\n"   /* number of arguments. */
+                  "pushl        %5\n"   /* system call number. */
+                  "int          %9"
+                  : 
+                  "=a" (return_value)
+                  :
+                  "g" (out_service),
+                  "g" (services),
+                  "g" (service_lookup),
+                  "g" (3),              /* number of arguments. */
+                  "g" (SYSTEM_CALL_SERVICE_LOOKUP),
+                  "N" (SYSTEM_CALL_IDT_ENTRY));
+    return return_value;
+}
+
+
+#endif /* (! defined __STORM_KERNEL__ ) &&
+          (! defined __STORM_KERNEL_MODULE__) */
 
 #endif /* !__STORM_IA32_SYSTEM_CALL_H__ */
