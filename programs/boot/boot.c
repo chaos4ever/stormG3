338238ef6ff18eabd65f50d1e978ac2302ef4c5c
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-28 22:27:55 +0000

    Updated the testing a bit.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@956 1f819a86-672b-0410-b5ed-9de735de380c

--- programs/boot/boot.c
+++ programs/boot/boot.c
@@ -1,4 +1,4 @@
-/* $chaos: boot.c,v 1.2 2002/10/24 22:15:57 per Exp $ */
+/* $chaos: boot.c,v 1.3 2002/10/27 20:30:05 per Exp $ */
 /* Abstract: Boot program (somewhat like init in Unix systems). */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -6,9 +6,12 @@
 /* Use freely under the terms listed in the file LICENSE. */
 
 #include <storm/storm.h>
+#include <kernel/kernel.h>
 
 void _start (void) NORETURN;
 int main (int argc UNUSED, char **argv UNUSED);
+
+char *test = "spam spam spam!";
 
 void _start (void)
 {
@@ -17,26 +20,23 @@ void _start (void)
    while (TRUE);
 }
 
-float q (float p);
-float q (float p)
-{
-    p += 20;
-    return p;
-}
-
-char *test = "spam spam spam!";
 
 /* Main function. */
 int main (int argc UNUSED, char **argv UNUSED)
 {
-    float p = 0;
+    size_t services;
+    service_t *service_list;
+    service_lookup_t service_lookup = 
+        {
+            "kernel", NULL, NULL, NULL, KERNEL_SERVICE_MAJOR_VERSION,
+            KERNEL_SERVICE_MINOR_VERSION
+        };
+    service_connection_id_t connection_id;
 
-    //    p = 1.5000902;
-    //    p += 8.983293;
-    q(p);
-    asm("push test\n"
-        "pushl $0x1\n"
-        "int $0x88");
-    // while (TRUE);
+    system_call_service_lookup (&service_lookup, &services, &service_list);
+    system_call_service_connect (service_list[0].id, &connection_id);
+    //    system_call_service_invoke (&connection_id, KERNEL_FUNCTION_DEBUG_PRINT,
+    //                                test);
+    //    system_call_service_close (connection_id);
     return 0;
 }
