49b39d5b92629f1ceda7906eaf36545bc905f32c
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-29 20:45:36 +0000

    Added support for numeric service IDs (unique).
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@960 1f819a86-672b-0410-b5ed-9de735de380c

--- stormG3/source/ia32/service.c
+++ stormG3/source/ia32/service.c
@@ -1,4 +1,4 @@
-/* $chaos: service.c,v 1.12 2002/10/28 07:52:18 per Exp $ */
+/* $chaos: service.c,v 1.13 2002/10/28 21:47:31 per Exp $ */
 /* Abstract: Service support. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -19,8 +19,27 @@ static service_data_t *first_service = NULL;
 /* Lock used to protect the service data structure. */
 static spinlock_t service_lock = SPIN_UNLOCKED;
 
+/* The next free service ID. We take for granted that this will not
+   wrap around. If you create ten new services every second, this
+   variable will wrap around after 58494241735 years. I doubt you will
+   have that kind of an uptime on a chaos machine. Please prove me
+   wrong! ;-) */
+static uint64_t service_id = 0;
+
+/* Create a new service ID. Service IDs are not reused, and that is a
+   design decision that we have made, so please don't file it as a
+   bug. It will not be changed. */
+static service_id_t service_create_id (void)
+{
+    service_id_t id = service_id;
+    service_id++;
+
+    return id;
+}
+
 /* Register a service provider. */
-return_t service_register (char *name, char *vendor, char *model, char *id,
+return_t service_register (char *name, char *vendor, char *model, 
+                           char *device_id,
                            unsigned int major_version,
                            unsigned int minor_version,
                            service_info_t service_info)
@@ -38,7 +57,7 @@ return_t service_register (char *name, char *vendor, char *model, char *id,
     if (string_length (name) + 1 > SERVICE_NAME_LENGTH ||
         string_length (vendor) + 1 > SERVICE_VENDOR_LENGTH ||
         string_length (model) + 1 > SERVICE_MODEL_LENGTH ||
-        string_length (id) + 1 > SERVICE_ID_LENGTH)
+        string_length (device_id) + 1 > SERVICE_ID_LENGTH)
     {
         debug_print ("%s: One of the fields were too long.\n", __FUNCTION__);
         return STORM_RETURN_INVALID_ARGUMENT; 
@@ -47,12 +66,14 @@ return_t service_register (char *name, char *vendor, char *model, char *id,
     string_copy (service->name, name);
     string_copy (service->vendor, vendor);
     string_copy (service->model, model);
-    string_copy (service->id, id);
+    string_copy (service->device_id, device_id);
     service->major_version = major_version;
     service->minor_version = minor_version;
     service->service_info = service_info;
 
+    /* Locked code (non-reentrant). */
     spin_lock (&service_lock);
+    service->id = service_create_id ();
     service->next = (struct service_data_t *) first_service;
     first_service = service;
     spin_unlock (&service_lock);
@@ -69,7 +90,7 @@ return_t service_unregister (char *service UNUSED,
 
 /* Lookup a service. */
 return_t service_lookup (const char *name, const char *vendor, 
-                         const char *model, const char *id,
+                         const char *model, const char *device_id,
                          unsigned int major_version, 
                          unsigned int minor_version, size_t *services,
                          service_t **out_service)
@@ -93,7 +114,7 @@ return_t service_lookup (const char *name, const char *vendor,
             string_compare (service->name, name) == 0 && 
             (vendor == NULL || string_compare (service->vendor, vendor) == 0) &&
             (model == NULL || string_compare (service->model, model) == 0) &&
-            (id == NULL || string_compare (service->id, id) == 0))
+            (device_id == NULL || string_compare (service->device_id, device_id) == 0))
         {
             (*services)++;
         }
@@ -129,12 +150,13 @@ return_t service_lookup (const char *name, const char *vendor,
             string_compare (service->name, name) == 0 && 
             (vendor == NULL || string_compare (service->vendor, vendor) == 0) &&
             (model == NULL || string_compare (service->model, model) == 0) &&
-            (id == NULL || string_compare (service->id, id) == 0))
+            (device_id == NULL || string_compare (service->device_id, device_id) == 0))
         {
             string_copy ((*out_service)[counter].name, service->name);
             string_copy ((*out_service)[counter].vendor, service->vendor);
             string_copy ((*out_service)[counter].model, service->model);
-            string_copy ((*out_service)[counter].id, service->id);
+            string_copy ((*out_service)[counter].device_id, service->device_id);
+            (*out_service)[counter].id = service->id;
             (*out_service)[counter].major_version = service->major_version;
             (*out_service)[counter].minor_version = service->minor_version;
             (*out_service)[counter].service_info = service->service_info;
