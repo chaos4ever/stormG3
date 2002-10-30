907b5b1c1f74b1ec64fa41cd29faafa28bd846f2
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-10-30 22:40:43 +0000

    Test the new syscalls.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@974 1f819a86-672b-0410-b5ed-9de735de380c

--- programs/boot/boot.c
+++ programs/boot/boot.c
@@ -1,4 +1,4 @@
-/* $chaos: boot.c,v 1.3 2002/10/27 20:30:05 per Exp $ */
+/* $chaos: boot.c,v 1.4 2002/10/28 22:27:55 per Exp $ */
 /* Abstract: Boot program (somewhat like init in Unix systems). */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -24,8 +24,8 @@ void _start (void)
 /* Main function. */
 int main (int argc UNUSED, char **argv UNUSED)
 {
-    size_t services;
-    service_t *service_list;
+    size_t services = 1;
+    service_t service_list;
     service_lookup_t service_lookup = 
         {
             "kernel", NULL, NULL, NULL, KERNEL_SERVICE_MAJOR_VERSION,
@@ -34,9 +34,9 @@ int main (int argc UNUSED, char **argv UNUSED)
     service_connection_id_t connection_id;
 
     system_call_service_lookup (&service_lookup, &services, &service_list);
-    system_call_service_connect (service_list[0].id, &connection_id);
+    system_call_service_connect (service_list.id, &connection_id);
     //    system_call_service_invoke (&connection_id, KERNEL_FUNCTION_DEBUG_PRINT,
     //                                test);
-    //    system_call_service_close (connection_id);
+    system_call_service_close (connection_id);
     return 0;
 }
