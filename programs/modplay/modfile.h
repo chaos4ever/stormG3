ccdfdb47338ee4d610189338bc6b1c345c64b281
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-11-13 16:38:26 +0000

    Added the mod player.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@983 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ programs/modplay/modfile.h
@@ -0,0 +1,12 @@
+/* $chaos: modfile.h,v 1.2 2002/05/23 11:22:52 per Exp $ */
+/* Copyright 2000, 2002 chaos development. */
+/* Author: Erik Moren <nemo@chaosdev.org> */
+
+/* Use freely under the terms listed in the file LICENSE. */
+
+#ifndef __MODFILE_H__
+#define __MODFILE_H__
+
+extern uint8_t modfile[];
+
+#endif /* !__MODFILE_H__ */
