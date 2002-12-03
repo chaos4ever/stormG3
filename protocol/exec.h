d57aa15daf1e57a15d4605d0d91866c17fa3a414
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-03 21:49:45 +0000

    Moved from libraries/exec/defines.h
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1106 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ protocol/exec.h
@@ -0,0 +1,28 @@
+/* $chaos: defines.h,v 1.2 2002/10/04 21:23:19 per Exp $ */
+/* Abstract: Definition of the exec protocol. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file LICENSE. */
+
+#ifndef __PROTOCOL_EXEC_H__
+#define __PROTOCOL_EXEC_H__
+
+/**
+ * @file                block.h
+ * @brief               Definition of the exec protocol.
+ */
+
+/**
+ * @brief               The major version of the exec service. 
+ */
+#define EXEC_PROTOCOL_MAJOR_VERSION \
+                        (0)
+
+/**
+ * @brief               The minor version of the exec protocol.
+ */
+#define EXEC_PROTOCOL_MINOR_VERSION \
+                        (1)
+
+#endif /* !__PROTOCOL_EXEC_H__ */
