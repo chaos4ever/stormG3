c23b26d0473cfabdb837c287d66572c80f407442
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-03 14:11:59 +0000

    Definition of the console protocol, moved party from libraries/console/defines.h.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1077 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ protocol/console.h
@@ -0,0 +1,32 @@
+/* $chaos: $ */
+/* Abstract: Definitions for the console protocol. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file LICENSE. */
+
+/**
+ * @file             console.h
+ * @brief            Definition of the console protocol.
+ */
+
+#ifndef __PROTOCOL_CONSOLE_H__
+#define __PROTOCOL_CONSOLE_H__
+
+/* A magic cookie, to make sure this is really a block service
+   provider. FIXME: Let this be truly unique. */
+#define CONSOLE_MAGIC_COOKIE              0x12345678
+
+/* The major version of the block service. Bump this one step when you
+   make CHANGES to the block service API that will make it
+   incompatible with old programs. */
+#define CONSOLE_PROTOCOL_MAJOR_VERSION \
+                        (0)
+
+/* The minor version of the block service. Should be bumped whenever a
+   function is added to the API that DOES NOT break the binary
+   compatibility with previously compiled programs. */
+#define CONSOLE_PROTOCOL_MINOR_VERSION \
+                        (1)
+
+#endif /* !__PROTOCOL_CONSOLE_H__ */
