b85052bd86d3a3b51578b5f2d7e5824d83c3a5cb
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-03 11:51:07 +0000

    compile.sh now creates relative symlinks for current-arch.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1071 1f819a86-672b-0410-b5ed-9de735de380c

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-# $chaos$
+# $chaos: compile.sh,v 1.7 2002/11/13 20:13:15 per Exp $
 # Abstract: Quick and dirty hack to compile everything.
 
 # The source root.
@@ -8,7 +8,10 @@ INSTALL_PREFIX=/tftpboot
 # Set up the link so we will know which architecture is the
 # current. FIXME: Add an architecture check perhaps?? :-)
 rm -f $ROOT/stormG3/include/storm/current-arch
-ln -sf $ROOT/stormG3/include/storm/ia32 $ROOT/stormG3/include/storm/current-arch
+#ln -sf $ROOT/stormG3/include/storm/ia32 $ROOT/stormG3/include/storm/current-arch
+cd $ROOT/stormG3/include/storm || exit
+ln -sf ia32 current-arch
+cd - || exit
 
 # Build stormG3
 cd $ROOT/stormG3 || exit
@@ -16,7 +19,10 @@ cons $STORM_OPTIONS || exit
 
 # Set up the link in the installed root as well.
 rm -f $INSTALL_PREFIX/data/programming/c/headers/storm/current-arch
-ln -sf $INSTALL_PREFIX/data/programming/c/headers/storm/ia32 $INSTALL_PREFIX/data/programming/c/headers/storm/current-arch
+#ln -sf $INSTALL_PREFIX/data/programming/c/headers/storm/ia32 $INSTALL_PREFIX/data/programming/c/headers/storm/current-arch
+cd $INSTALL_PREFIX/data/programming/c/headers/storm || exit
+ln -sf ia32 current-arch
+cd - || exit
 
 # Build the rest (order is important).
 cd $ROOT/libraries
