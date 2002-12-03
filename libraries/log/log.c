5af60a3e158a1c5f2a81c04cefbccb9112e3d5d1
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-03 14:32:48 +0000

    Fixed to use the new service_lookup
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1089 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/log/log.c
+++ libraries/log/log.c
@@ -1,4 +1,4 @@
-/* $chaos: log.c,v 1.7 2002/10/28 08:08:15 per Exp $ */
+/* $chaos: log.c,v 1.9 2002/11/20 20:04:31 per Exp $ */
 /* Abstract: Log library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -13,9 +13,15 @@ return_t log_lookup (log_service_t *log)
     // FIXME: Lame.
     size_t services = 1;
     service_t service;
+    service_lookup_t lookup;
 
     /* Find the log service. */
-    if (service_lookup ("log", NULL, NULL, NULL, LOG_SERVICE_MAJOR_VERSION, LOG_SERVICE_MINOR_VERSION, &services, &service) != STORM_RETURN_SUCCESS)
+
+    lookup.protocol_name = "log";
+    lookup.major_version = LOG_PROTOCOL_MAJOR_VERSION;
+    lookup.minor_version = LOG_PROTOCOL_MINOR_VERSION;
+
+    if (service_lookup (&lookup, &services, &service) != STORM_RETURN_SUCCESS)
     {
         debug_print ("Failed to lookup log service provider.\n");
         return LOG_RETURN_SERVICE_UNAVAILABLE;
@@ -30,9 +36,9 @@ return_t log_lookup (log_service_t *log)
 return_t log_register (service_register_t *service_register_info,
                          service_method_t *service_method)
 {    
-    service_register_info->service_name = "log";
-    service_register_info->major_version = LOG_SERVICE_MAJOR_VERSION;
-    service_register_info->minor_version = LOG_SERVICE_MINOR_VERSION;
+    service_register_info->protocol_name = "log";
+    service_register_info->major_version = LOG_PROTOCOL_MAJOR_VERSION;
+    service_register_info->minor_version = LOG_PROTOCOL_MINOR_VERSION;
 
     return service_register (service_register_info, service_method);
 }
