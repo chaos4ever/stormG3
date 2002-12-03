742ec690a6b82ec377a4acc68247e9389372fdf3
johannes <johannes@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-03 14:27:12 +0000

    Include <protocol/filesystem.h> instead of defines.h.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1086 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/filesystem/filesystem.h
+++ libraries/filesystem/filesystem.h
@@ -1,4 +1,4 @@
-/* $chaos: filesystem.h,v 1.1 2002/07/21 12:38:40 per Exp $ */
+/* $chaos: filesystem.h,v 1.2 2002/10/04 19:00:52 per Exp $ */
 /* Abstract: Function prototypes and structure definitions for the
    filesystem library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
@@ -10,7 +10,7 @@
 #define __FILESYSTEM_FILESYSTEM_H__
 
 #include <storm/storm.h>
-#include <filesystem/defines.h>
+#include <protocol/filesystem.h>
 #include <filesystem/return_values.h>
 #include <filesystem/types.h>
 #include <filesystem/functions.h>
