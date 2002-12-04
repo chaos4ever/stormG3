621fb496455bba028dc6024188d40298279484ae
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-04 21:51:51 +0000

    Improved log_lookup () a little.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1112 1f819a86-672b-0410-b5ed-9de735de380c

--- libraries/log/log.c
+++ libraries/log/log.c
@@ -1,4 +1,4 @@
-/* $chaos: log.c,v 1.9 2002/11/20 20:04:31 per Exp $ */
+/* $chaos: log.c,v 1.10 2002/12/03 14:32:48 johannes Exp $ */
 /* Abstract: Log library. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -8,18 +8,22 @@
 #include <log/log.h>
 
 /* Get a connection to the log service provider. */
+// FIXME: Take a service_lookup_t parameter.
 return_t log_lookup (log_service_t *log)
 {
     // FIXME: Lame.
     size_t services = 1;
     service_t service;
-    service_lookup_t lookup;
-
-    /* Find the log service. */
-
-    lookup.protocol_name = "log";
-    lookup.major_version = LOG_PROTOCOL_MAJOR_VERSION;
-    lookup.minor_version = LOG_PROTOCOL_MINOR_VERSION;
+    service_lookup_t lookup =
+        {
+            "log",      /* protocol_name */
+            NULL,       /* service_vendor */
+            NULL,       /* device_vendor */
+            NULL,       /* model */
+            NULL,       /* device_id */
+            LOG_PROTOCOL_MAJOR_VERSION,         /* major version */
+            LOG_PROTOCOL_MINOR_VERSION,         /* minor version */
+        };
 
     if (service_lookup (&lookup, &services, &service) != STORM_RETURN_SUCCESS)
     {
