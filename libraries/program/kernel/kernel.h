e11882f7d780613ce770fe22b5de38758f409215
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-12 22:08:09 +0000

    New headers, used by the non-existing program libraries.
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1139 1f819a86-672b-0410-b5ed-9de735de380c

---
+++ libraries/program/kernel/kernel.h
@@ -0,0 +1,22 @@
+/* $chaos: kernel.h,v 1.1 2002/12/08 20:09:49 per Exp $ */
+/* Abstract: Kernel library. */
+/* Author: Per Lundberg <per@chaosdev.org> */
+
+/* Copyright 2002 chaos development. */
+/* Use freely under the terms listed in the file LICENSE. */
+
+/**
+ * @file             kernel.h
+ * @brief            Kernel library.
+ */
+
+#ifndef __KERNEL_KERNEL_H__
+#define __KERNEL_KERNEL_H__
+
+#include <storm/storm.h>
+#include <protocol/kernel.h>
+#include <kernel/return_values.h>
+#include <kernel/types.h>
+#include <kernel/functions.h>
+
+#endif /* !__KERNEL_KERNEL_H__ */
