b1b9e1ba718a4816437508d66e1d2ade96559d7b
per <per@1f819a86-672b-0410-b5ed-9de735de380c>
2002-12-20 20:49:27 +0000

    Last checkin this year: happy new year!
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1161 1f819a86-672b-0410-b5ed-9de735de380c

--- protocol/video.h
+++ protocol/video.h
@@ -1,4 +1,4 @@
-/* $chaos: $ */
+/* $chaos: video.h,v 1.1 2002/12/03 14:45:42 johannes Exp $ */
 /* Abstract: Video protocol defines. */
 /* Author: Per Lundberg <per@chaosdev.org> */
 
@@ -6,24 +6,48 @@
 /* Use freely under the terms listed in the file LICENSE. */
 
 /**
- * @file             video.h
+ * @file             protocol/video.h
  * @brief            Definition of the video protocol.
  */
 
 #ifndef __PROTOCOL_VIDEO_H__
 #define __PROTOCOL_VIDEO_H__
 
-/* The version of the video service. Increase when you make
-   binary-incompatible changes. That way, all clients trying to
-   connect to your service will fail. */
+/**
+ * @brief               Video protocol major version.
+ *
+ * The version of the video service. Increase when you make
+ * binary-incompatible changes. That way, all clients trying to
+ * connect to your service will fail. 
+ */
 #define VIDEO_PROTOCOL_MAJOR_VERSION \
                         (0)
 
-/* The minor version is used for handling dependencies when adding
-   binary-compatible changes (like adding a new function). A program
-   using the new function needs to have a new minor version to depend
-   on. */
+/**
+ * @brief               Video protocol minor version.
+ *
+ * The minor version is used for handling dependencies when adding
+ * binary-compatible changes (like adding a new function). A program
+ * using the new function needs to have a new minor version to depend
+ * on. 
+ */
 #define VIDEO_PROTOCOL_MINOR_VERSION \
                         (1)
+
+/**
+ * @brief               Video mode types.
+ */
+enum video_mode_t 
+{
+    /**
+     * @brief           Text-based video modes.
+     */
+    VIDEO_MODE_TYPE_TEXT,
+
+    /**
+     * @brief           Graphical video modes.
+     */
+    VIDEO_MODE_TYPE_GRAPHIC,
+};
 
 #endif /* !__PROTCOL_VIDEO_H__ */
