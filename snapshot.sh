ee94f53ea4ca7abf36f6f08dbfa9cd25c46b470f
perlundberg <perlundberg@1f819a86-672b-0410-b5ed-9de735de380c>
2006-07-11 20:29:51 +0000

    Fixed bug
    git-svn-id: https://svn.code.sf.net/p/chaos/svn/trunk/chaos@1204 1f819a86-672b-0410-b5ed-9de735de380c

--- snapshot.sh
+++ snapshot.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 cd ..
+rm -rf chaos-$(date +%Y%m%d) 
 mkdir chaos-$(date +%Y%m%d) || exit 1
 echo "Copying..."
 cp -r chaos/* chaos-$(date +%Y%m%d) || exit 1
 echo "Cleaning..."
-cd chaos-$(date +%Y%m%d) && ./clean.sh
+cd chaos-$(date +%Y%m%d) && ./clean.sh && cd ..
 echo "Creating archive..."
 tar --exclude CVS -cjf chaos-$(date +%Y%m%d).tar.bz2 chaos-$(date +%Y%m%d)
 rm -rf chaos-$(date +%Y%m%d) || exit 1
